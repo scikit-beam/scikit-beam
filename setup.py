27b3d4f6d9c108fe8bc3142b147915913ddbf973
Thomas A Caswell <tcaswell@bnl.gov>
2014-05-07 16:40:13 -0400

    BLD : added basic setup.py script

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import setuptools
+from distutils.core import setup
+
+setup(
+    name='NSLS2',
+    version='0',
+    author='Brookhaven National Lab',
+    packages=["nsls2"],
+    )
