f2c4d91fa58f3fa86006903689df74c23c8f655a
Eric Dill <edill@bnl.gov>
2014-06-02 14:54:19 -0400

    Narrowly avoided the foot cannon

--- nsls2/ex/reciprocal_space_reconstruction_pipeline.py
+++ nsls2/ex/reciprocal_space_reconstruction_pipeline.py
@@ -8,9 +8,12 @@ Created on May 29, 2014
 def run():
     from nsls2.io.binary import read_binary
     from nsls2.core import detector2D_to_1D
+    from nsls2.recip import project_to_sphere
     import numpy as np
     from matplotlib import pyplot
-    fname = "/home/edill/Data/twinned cbr4/cbr4_singlextal_rotate190_50deg_2s_90kev_203f.cor.101.cor"
+    import matplotlib
+    from mpl_toolkits.mplot3d import Axes3D
+    fname = "nsls2/ex/data/recip_space_recon/cbr4_singlextal_rotate190_50deg_2s_90kev_203f.cor.042.cor"
     params = {"filename": fname,
             "nx": 2048,
             "ny": 2048,
@@ -21,14 +24,23 @@ def run():
             "wavelength": .13702,
             "detector_center": (1033.321, 1020.208),
             "dist_sample": 188.672,
-            "pixel_size": (200, 200)
+            "pixel_size": (.200, .200)
             }
     # read in a binary file
     data, header = read_binary(**params)
 
-    list_1D = detector2D_to_1D(data, **params)
+    # list_1D = detector2D_to_1D(data, **params)
 
-    pyplot.imshow(data)
+    qi = project_to_sphere(data, ROI=[900,1100,900,1100], **params)
+    
+    # normalize the intensity to between 0 and 1
+    qi[:,3] /= max(qi[:,3])
+    
+    fig = pyplot.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.view_init(90, 0)
+    ax.scatter3D(qi[:,0], qi[:,1], qi[:,2], c=qi[:,3], s=.1)
+    pyplot.show()
 
 if __name__ == "__main__":
     run()
--- nsls2/recip.py
+++ nsls2/recip.py
@@ -11,3 +11,94 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import six
+import numpy as np
+
+def project_to_sphere(img, dist_sample, detector_center, pixel_size, wavelength, 
+                      ROI = None, **kwargs):
+    """
+    Project the pixels on the 2D detector to the surface of a sphere.
+    
+    Parameters
+    ----------
+    img: ndarray
+         2D detector image
+    dist_sample: float
+                 see keys_core
+                 (mm)
+    detector_center: 2 element float array
+                     see keys_core
+                     (pixels)
+    pixel_size: 2 element float array
+                see keys_core
+                (mm)
+    wavelength: float
+                see keys_core
+                (Angstroms)
+    ROI: 4 element int array
+           ROI defines a rectangular ROI for img
+           ROI[0] == x_min
+           ROI[1] == x_max
+           ROI[2] == y_min
+           ROI[3] == y_max
+    **kwargs: dict
+              Bucket for extra parameters from an unpacked dictionary
+    
+    Returns
+    -------
+    qi: 4 x N array of the coordinates in Q space (A^-1)
+        Rows correspond to individual pixels
+        Columns are (Qx, Qy, Qz, I)
+    """
+    
+    if ROI is not None and len(ROI) == 4:
+        # slice the image based on the desired ROI
+        img=img[ROI[0]:ROI[1], ROI[2]:ROI[3]]
+        
+    # create the array of x indices
+    arr_2d_x = np.zeros((img.shape[0], img.shape[1]), dtype=np.float)
+    for x in range(img.shape[0]):
+        arr_2d_x[x:x + 1] = x + 1 + ROI[0]
+
+    # create the array of y indices
+    arr_2d_y = np.zeros((img.shape[0], img.shape[1]), dtype=np.float)
+    for y in range(img.shape[1]):
+        arr_2d_y[:, y:y + 1] = y + 1 + ROI[2]
+
+    # subtract the detector center 
+    arr_2d_x -= detector_center[0]
+    arr_2d_y -= detector_center[1]
+    
+    # convert the pixels into real-space dimensions
+    arr_2d_x *= pixel_size[0]
+    arr_2d_y *= pixel_size[1] 
+
+    print("Image shape: {0}".format(img.shape))
+    # define a new 4 x N array
+    qi = np.zeros((4,) + (img.shape[0] * img.shape[1],))
+    # fill in the x coordinates
+    qi[0] = arr_2d_x.flatten()
+    # fill in the y coordinates
+    qi[1] = arr_2d_y.flatten()
+    # set the z coordinate for all pixels to the distance from the sample to the detector
+    qi[2].fill(dist_sample)
+    # fill in the intensity values of the pixels
+    qi[3] = img.flatten()
+    # convert to an N x 4 array
+    qi = qi.transpose()
+    # compute the unit vector of each pixel
+    qi[:,0:2] = qi[:,0:2]/np.linalg.norm(qi[:,0:2])
+    # convert the pixel positions from real space distances into the reciprocal space
+    # vector, Q
+    Q = 4 * np.pi / wavelength * np.sin(np.arctan(qi[:,0:2]))
+    # project the pixel coordinates onto the surface of a sphere of radius dist_sample
+    qi[:,0:2] *= dist_sample
+    # compute the vector from the center of the detector (i.e., the zero of reciprocal
+    # space) to each pixel
+    qi[:,2] -= dist_sample
+    # compute the unit vector for each pixels position relative to the center of the 
+    # detector, but now on the surface of a sphere
+    qi[:,0:2] = qi[:,0:2]/np.linalg.norm(qi[:,0:2])
+    # convert to reciprocal space
+    qi[:,0:2] *= Q
+    
+    return qi
