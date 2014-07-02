6fa6429c21e0812499d24c439d3598f6fa13cce2
Sameera Abeykoon <sameera@bnl.gov>
2014-07-02 10:48:09 -0400

    Added reduced reprsentation(Statistics) of 2D X-ray images to core.py

--- nsls2/core.py
+++ nsls2/core.py
@@ -259,3 +259,27 @@ def img_subtraction_pre(img_arr, is_reference):
 
     # return the output
     return corrected_image
+
+def RR1Choice(image_data):
+    '''
+    This Modeule is for
+    Statistics or Reduced Representation of 2D images
+    (Mean, Total Intensity, Standard Deviation )
+    
+    Parameters
+    ----------
+    image_data : ndarray
+    MxN array of data
+     
+    Returns
+    -------
+    Reduced Representation Choices
+    --------------------------------
+    rrm : Mean
+    rrt : Total Intensity
+    rrs : Standard Deviation
+    '''
+    rrm = np.mean(image_data)
+    rrt = np.sum(image_data)
+    rrs = np.sum(image_data)
+    return rrm, rst, rrs
