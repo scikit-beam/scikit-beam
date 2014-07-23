221cf2d7c11a54c8a28ac63d2b14f9eb2d372ed4
Iltis <giltis@freyja.nsls2.bnl.gov>
2014-07-23 18:09:23 -0400

    DEV: Added functions for parsing AmiraMesh header into md_dict
    Created two new functions:
    1) sort_amira_header: distils original header list  down to only
    information that should be included in the header. Removes all new
    line characters and white space.
    2) create_md_dict: extracts all data pertinent to the loaded file
    and generates a dictionary of all the important metadata.

--- nsls2/io/avizo_io.py
+++ nsls2/io/avizo_io.py
@@ -26,7 +26,7 @@ def read_amira_header {}{
     }
 
 #Reference am files:
-fname_orig = '/home/giltis/Dropbox/BNL_Docs/Alt_File_Formats/am_FILES/raw_cnvrted/Control_column_1_DI_A_Above-Edge.am'
+fname_orig = '/home/giltis/Dropbox/BNL_Docs/Alt_File_Formats/am_cnvrt_compare/Shew_C5_bio_abv.am'
 def read_amira (src_file):
     f = open(src_file, 'r')
     while True:
@@ -44,6 +44,13 @@ def read_amira (src_file):
     except:
         am_data = []
     am_data = f.read()
+    f.close()
+    return am_header, am_data
+
+def cnvrt_amira_data_2numpy (data, header):
+    Zdim = *from_header*
+    Ydim = *from_header*
+    Xdim = *from_header*
     #Strip out null characters from the string of binary values
     data_strip = am_data.strip('\n')
     flt_values = np.fromsstring(data_strip, '<f4')
@@ -51,28 +58,47 @@ def read_amira (src_file):
     flt_values.resize(Zdim, Ydim, Xdim)
     return am_header, flt_values
 
+def sort_amira_header (header_list):
+    for row in range(len(header_list)):
+        header_list[row] = header_list[row].strip('\n')
+        header_list[row] = header_list[row].split(" ")
+        for column in range(len(header_list[row])):
+            header_list[row] = filter(None, header_list[row])
+    header_list = filter(None, header_list)
+    return header_list
 
 def create_md_dict (header_list):
-    for _ in range(len(header_list)):
-        head_list[_] = header_list[_].strip
-        head_list[_] = header_list[_].split(" ")
-    md_dict = {
-        'software_src' : head_list[0][1],
-        'data_format' : head_list[0][2],
-        'data_format_version' : head_list[0][3]
-        'array_dims' : {'x_dim' : int(head_list[3][2]),
-                        'y_dim' : int(head_list[3][3]),
-                        'z_dim' : int(head_list[3][4])
-                        },
-        'data_type' : head_list[6][2],
-        'bounding_box' : {'x_min' : head_list[7][1],
-                          'x_max' : head_list[7][2],
-                          'y_min' : head_list[7][3],
-                          'y_max' : head_list[7][4],
-                          'z_min' : head_list[7][5],
-                          'z_max' : head_list[7][6]
-                          },
-        'coordinate_type' : 'x_min' : head_list[8][1],
+    md_dict = {'software_src' : header_list[0][1],
+               'data_format' : header_list[0][2],
+               'data_format_version' : header_list[0][3]
+                }
+    for row in range(len(header_list)):
+        try:
+            md_dict['array_dims'] = {'x_dim' : int(header_list[row][header_list[row].index('define') + 2]),
+                                     'y_dim' : int(header_list[row][header_list[row].index('define') + 3]),
+                                     'z_dim' : int(header_list[row][header_list[row].index('define') + 4])
+                                     }
+        except:
+        #    continue
+            try:
+                md_dict['data_type'] = header_list[row][header_list[row].index('Content') + 2]
+            except: 
+            #    continue
+                try:
+                    md_dict['coord_type'] = header_list[row][header_list[row].index('CoordType') + 1]
+                except:
+                    try:
+                        md_dict['bounding_box'] = {'x_min' : float(header_list[row][header_list[row].index('BoundingBox') + 1]),
+                                                   'x_max' : float(header_list[row][header_list[row].index('BoundingBox') + 2]),
+                                                   'y_min' : float(header_list[row][header_list[row].index('BoundingBox') + 3]),
+                                                   'y_max' : float(header_list[row][header_list[row].index('BoundingBox') + 4]),
+                                                   'z_min' : float(header_list[row][header_list[row].index('BoundingBox') + 5]),
+                                                   'z_max' : float(header_list[row][header_list[row].index('BoundingBox') + 6])
+                                                   }
+                    except:
+                        continue
+    return md_dict
+
                               '
         
 
