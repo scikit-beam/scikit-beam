4eada95b18fdb9c644161ce6bc2bde20eb71f842
Sameera Abeykoon <sameera@bnl.gov>
2014-07-30 14:10:55 -0400

    modified the test_recip.py add the test_process_grid part to check the process_grid function in the recip.py is working.

--- nsls2/tests/test_recip.py
+++ nsls2/tests/test_recip.py
@@ -1,4 +1,3 @@
-from pylab import *
 import numpy as np
 import nsls2.recip as recip
 
@@ -21,45 +20,46 @@ def test_process_to_q():
     # delta=40, theta=15, chi = 90, phi = 30, mu = 10.0, gamma=5.0
     
     istack = 100*np.ones((settingAngles.shape[0], 256, 256))
-    # print istack
-    #istack = 100*np.ones((settingAngles.shape[0], detPixSizeX, detPixSizeY))
 
     totSet = recip.process_to_q(settingAngles, detSizeX, detSizeY, detPixSizeX, detPixSizeY, detX0, detY0, detDis, waveLen, UBmat, istack)
     
+    print totSet print "\n\n Six Angles "
+    print settingAngles
+    print "\n\n Known HKL values  "
+    print " HKL = [[-0.15471196  0.19673939 -0.11440936]]"
+    print " HKL = [[ 0.10205953  0.45624416 -0.27200778]]"
+    
     print " \n\n HKL Values "
     print totSet
-    return totSet
+   
 
-
-"""def test_process_grid():
-    size = 10
-    data = test_process_to_q()
+def test_process_grid():
+    size = 5
     Qmax = array([1.0, 1.0, 1.0])
     Qmin = array([-1.0, -1.0, -1.0])
     dQN = array([size,size, size])
+    sigma = 0.1
     grid = np.mgrid[0:dQN[0], 0:dQN[1], 0:dQN[2]]
     r = (Qmax - Qmin) / dQN
-    
+
     X = grid[0] * r[0] + Qmin[0]
     Y = grid[1] * r[1] + Qmin[1]
     Z = grid[2] * r[2] + Qmin[2]
+
+    out = np.zeros((size,size,size))
     
-    # data = np.exp(-(X**2 + Y**2 + Z**2) / (2 * sigma**2))
-    #data = 1000*np.random.rand(size*size*size)
-    #data  = 100*np.ones(size*size*size)
-    
+    out = np.exp(-(X**2 + Y**2 + Z**2) / (2 * sigma**2))
+
     out = array([np.ravel(X),
                  np.ravel(Y),
                  np.ravel(Z),
-                 np.ravel(data)])
-    print " out"
-    print out
-    print out.shape
-                 
-    #gridData, gridOccu, gridStd, gridOut, emptNb, gridbins = recip.process_grid(out, Qmax, Qmin, dQN)
-    gridData, gridOccu, gridStd, gridOut = ctrans.grid3d(out, Qmin, Qmax, dQN, norm=1)
-                 # print "gridout = ", gridData,
-                 #print emptNb
-    return gridData, gridOccu, gridOut"""
+                 np.ravel(out)])
+    data = out.T
 
-
+    gridData, gridOccu, gridStd, gridOut, emptNb, gridbins = recip.process_grid(data,
+                                                array([-1.0, -1.0, -1.0]),
+                                                array([1.0, 1.0, 1.0]),
+                                                array([size, size, size]))
+    print "\n\n gridData "
+    print gridData
+    
