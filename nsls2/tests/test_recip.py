b2681c3dc57fafc1a478c3e271b062d410788e7f
Sameera Abeykoon <sameera@bnl.gov>
2014-07-30 16:58:45 -0400

    BUG : fixed the test_recip.py

--- nsls2/tests/test_recip.py
+++ nsls2/tests/test_recip.py
@@ -1,19 +1,21 @@
 import numpy as np
 import nsls2.recip as recip
-from numpy.testing import assert_array_equal, assert_array_almost_equal
- 
+import numpy.testing as npt
+
+
 def test_process_to_q():
 
-    detPixSizeX, detPixSizeY, detSizeX, detSizeY, detX0, detY0, detDis, detAng = 0.0135*8, 0.0135*8, 256, 256, 256/2.0, 256/2.0, 355.0, 0.0
-
+    detPixSizeX, detPixSizeY, detSizeX, detSizeY = 0.0135*8, 0.0135*8, 256, 256
+    detX0, detY0, detDis, detAng = 256/2.0, 256/2.0, 355.0, 0.0
+    
     energy = 640  # ( ev)
     # HC_OVER_E to convert from E to Lambda
     hc_over_e = 12398.4
-    waveLen = hc_over_e/ energy # (Angstrom )
+    waveLen = hc_over_e / energy  # (Angstrom )
      
-    UBmat= np.matrix([[-0.01231028454, 0.7405370482 , 0.06323870032],
-                       [ 0.4450897473 , 0.04166852402,-0.9509449389 ],
-                       [-0.7449130975 , 0.01265920962,-0.5692399963 ]])
+    UBmat = np.matrix([[-0.01231028454, 0.7405370482, 0.06323870032],
+                       [0.4450897473, 0.04166852402, -0.9509449389],
+                       [-0.7449130975, 0.01265920962, -0.5692399963]])
      
     settingAngles = np.matrix([[40., 15., 30., 25., 10., 5.],
                               [90., 60., 0., 30., 10., 5.]])
@@ -21,24 +23,33 @@ def test_process_to_q():
     
     istack = 100*np.ones((settingAngles.shape[0], 256, 256))
 
-    totSet = recip.process_to_q(settingAngles, detSizeX, detSizeY, detPixSizeX, detPixSizeY, detX0, detY0, detDis, waveLen, UBmat, istack)
+    totSet = recip.process_to_q(settingAngles, detSizeX, detSizeY,
+                                detPixSizeX, detPixSizeY, detX0, detY0,
+                                detDis, waveLen, UBmat, istack)
     
-    """print totSet print "\n\n Six Angles "
-    print settingAngles
-    print "\n\n Known HKL values  "
-    print " HKL = [[-0.15471196  0.19673939 -0.11440936]]"
-    print " HKL = [[ 0.10205953  0.45624416 -0.27200778]]"
+    #  Known HKL values for the given six angles)
+    HKL1 = np.matrix([[-0.15471196, 0.19673939,
+                                  -0.11440936]])
+    HKL2 = np.matrix([[0.10205953,  0.44624416,
+                                  -0.27200778]])
     
-    print " \n\n HKL Values "
-    print totSet"""
-   
+    # New HKL values obtained from the process_to_q
+    N_HKL1 = np.matrix([[totSet[0, 0], totSet[0, 1],
+                                    totSet[0, 2]]])
+    N_HKL2 = np.matrix([[totSet[-1, 0], totSet[-1, 1],
+                                    totSet[-1, 2]]])
+
+    # check the values are as expected
+    npt.assert_array_equal(HKL1, N_HKL1)
+    npt.assert_array_equal(HKL2, N_HKL2)
+
 
 def test_process_grid():
     size = 4
     sigma = 0.1
     Qmax = np.array([1.0, 1.0, 1.0])
     Qmin = np.array([-1.0, -1.0, -1.0])
-    dQN = np.array([size,size, size])
+    dQN = np.array([size, size, size])
     
     grid = np.mgrid[0:dQN[0], 0:dQN[1], 0:dQN[2]]
     r = (Qmax - Qmin) / dQN
@@ -47,19 +58,23 @@ def test_process_grid():
     Y = grid[1] * r[1] + Qmin[1]
     Z = grid[2] * r[2] + Qmin[2]
 
-    out = np.zeros((size,size,size))
+    out = np.zeros((size, size, size))
     
     out = np.exp(-(X**2 + Y**2 + Z**2) / (2 * sigma**2))
 
     data = np.array([np.ravel(X),
-                 np.ravel(Y),
-                 np.ravel(Z),
-                 np.ravel(out)])
+                     np.ravel(Y),
+                     np.ravel(Z),
+                     np.ravel(out)])
     data = data.T
 
-    gridData, gridOccu, gridStd, gridOut, emptNb, gridbins = recip.process_grid(data,
-                                                Qmax, Qmin, dQN)
-                                                
+    gridData, gridOccu, gridStd, gridOut, emptNb, gridbins = recip.process_grid(data, Qmax, Qmin, dQN)
+    
+    # Values that have to go to the gridder
+    Databack = np.ravel(out)
+    
+    # Values from the gridder
     gridDataback = np.ravel(gridData)
-    Databack =  np.ravel(out)
-    assert_array_almost_equal(gridDataback, Databack)
+    
+    # check the values are as expected
+    npt.assert_array_almost_equal(gridDataback, Databack)
