5db46b241ee9f214041f863db8449946c3803770
Sameera Abeykoon <sameera@bnl.gov>
2014-08-12 16:47:23 -0400

    MNT: modifed nsls2/tests/test_recip.py
    Changed variable names in test_recip.py

--- nsls2/tests/test_recip.py
+++ nsls2/tests/test_recip.py
@@ -29,23 +29,23 @@ def test_process_to_q():
                               [90., 60., 0., 30., 10., 5.]])
     # delta=40, theta=15, chi = 90, phi = 30, mu = 10.0, gamma=5.0
 
-    totSet = recip.process_to_q(setting_angles, detector_size, pixel_size, calibrated_center,
+    tot_set = recip.process_to_q(setting_angles, detector_size, pixel_size, calibrated_center,
                                 dist_sample, wavelength, ub_mat)
 
     #  Known HKL values for the given six angles)
-    HKL1 = np.matrix([[-0.15471196, 0.19673939, -0.11440936]])
-    HKL2 = np.matrix([[0.10205953,  0.45624416, -0.27200778]])
+    hkl1 = np.matrix([[-0.15471196, 0.19673939, -0.11440936]])
+    hkl2 = np.matrix([[0.10205953,  0.45624416, -0.27200778]])
 
     # New HKL values obtained from the process_to_q
 
-    N_HKL1 = np.around(np.matrix([[totSet[32896, 0],
-                    totSet[32896, 1], totSet[32896, 2]]]), decimals=8)
-    N_HKL2 = np.around(np.matrix([[totSet[98432, 0],
-                    totSet[98432, 1], totSet[98432, 2]]]), decimals=8)
+    n_hkl1 = np.around(np.matrix([[tot_set[32896, 0],
+                    tot_set[32896, 1], tot_set[32896, 2]]]), decimals=8)
+    n_hkl2 = np.around(np.matrix([[tot_set[98432, 0],
+                    tot_set[98432, 1], tot_set[98432, 2]]]), decimals=8)
 
     # check the values are as expected
-    npt.assert_array_equal(HKL1, N_HKL1)
-    npt.assert_array_equal(HKL2, N_HKL2)
+    npt.assert_array_equal(hkl1, n_hkl1)
+    npt.assert_array_equal(hkl2, n_hkl2)
 
 
 def test_process_grid():
@@ -53,16 +53,16 @@ def test_process_grid():
         return
     size = 4
     sigma = 0.1
-    Qmax = np.array([1.0, 1.0, 1.0])
-    Qmin = np.array([-1.0, -1.0, -1.0])
-    dQN = np.array([size, size, size])
+    q_max = np.array([1.0, 1.0, 1.0])
+    q_min = np.array([-1.0, -1.0, -1.0])
+    dqn = np.array([size, size, size])
 
-    grid = np.mgrid[0:dQN[0], 0:dQN[1], 0:dQN[2]]
-    r = (Qmax - Qmin) / dQN
+    grid = np.mgrid[0:dqn[0], 0:dqn[1], 0:dqn[2]]
+    r = (q_max - q_min) / dqn
 
-    X = grid[0] * r[0] + Qmin[0]
-    Y = grid[1] * r[1] + Qmin[1]
-    Z = grid[2] * r[2] + Qmin[2]
+    X = grid[0] * r[0] + q_min[0]
+    Y = grid[1] * r[1] + q_min[1]
+    Z = grid[2] * r[2] + q_min[2]
 
     out = np.zeros((size, size, size))
 
@@ -73,13 +73,13 @@ def test_process_grid():
                      np.ravel(Z)])
     data = data.T
 
-    gridData, gridOccu, gridStd, gridOut, emptNb, gridbins = recip.process_grid(data, out, Qmax, Qmin, dQN)
+    grid_data, grid_occu, grid_std, grid_out, empt_nb, gridbins = recip.process_grid(data, out, q_max, q_min, dqn)
     
     # Values that have to go to the gridder
-    Databack = np.ravel(out)
+    databack = np.ravel(out)
 
     # Values from the gridder
-    gridDataback = np.ravel(gridData)
+    grid_databack = np.ravel(grid_data)
 
     # check the values are as expected
-    npt.assert_array_almost_equal(gridDataback, Databack)
+    npt.assert_array_almost_equal(grid_databack, databack)
