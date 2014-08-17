11c778bc0b58102bb136c82747a8f17c41be2cc2
licode <mikeli7233@gmail.com>
2014-08-17 01:14:01 -0400

    add element data file to save data as dict

--- nsls2/fitting/base/element.py
+++ nsls2/fitting/base/element.py
@@ -1,73 +1,72 @@
-from collections import (Mapping, OrderedDict)
-#from collections import OrderedDict
+from collections import Mapping
 import six
 
-import xraylib
-
-
-line_name = ['Ka1', 'Ka2', 'Kb1', 'Kb2', 'La1', 'La2', 'Lb1', 'Lb2', 'Lb3', 'Lb4', 'Lb5',
-             'Lg1', 'Lg2', 'Lg3', 'Lg4', 'Ll', 'Ln', 'Ma1', 'Ma2', 'Mb', 'Mg']
-line_list = [xraylib.KA1_LINE, xraylib.KA2_LINE, xraylib.KB1_LINE, xraylib.KB2_LINE,
-             xraylib.LA1_LINE, xraylib.LA2_LINE,
-             xraylib.LB1_LINE, xraylib.LB2_LINE, xraylib.LB3_LINE, xraylib.LB4_LINE, xraylib.LB5_LINE,
-             xraylib.LG1_LINE, xraylib.LG2_LINE, xraylib.LG3_LINE, xraylib.LG4_LINE,
-             xraylib.LL_LINE, xraylib.LE_LINE,
-             xraylib.MA1_LINE, xraylib.MA2_LINE, xraylib.MB_LINE, xraylib.MG_LINE]
-line_dict = dict(zip(line_name, line_list))
-
-
-bindingE = ['K', 'L1', 'L2', 'L3', 'M1', 'M2', 'M3', 'M4', 'M5',
-            'N1', 'N2', 'N3', 'N4', 'N5', 'N6', 'N7', 'O1', 'O2', 'O3', 'O4', 'O5', 'P1', 'P2', 'P3']
-shell_list = [xraylib.K_SHELL, xraylib.L1_SHELL, xraylib.L2_SHELL, xraylib.L3_SHELL,
-              xraylib.M1_SHELL, xraylib.M2_SHELL, xraylib.M3_SHELL, xraylib.M4_SHELL, xraylib.M5_SHELL,
-              xraylib.N1_SHELL, xraylib.N2_SHELL, xraylib.N3_SHELL, xraylib.N4_SHELL,
-              xraylib.N5_SHELL, xraylib.N6_SHELL, xraylib.N7_SHELL,
-              xraylib.O1_SHELL, xraylib.O2_SHELL, xraylib.O3_SHELL, xraylib.O4_SHELL, xraylib.O5_SHELL,
-              xraylib.P1_SHELL, xraylib.P2_SHELL, xraylib.P3_SHELL]
-shell_dict = dict(zip(bindingE, shell_list))
-
-
-_XRAYLIB_MAP = {'lines': (line_dict, xraylib.LineEnergy),
-                'cs': (line_dict, xraylib.CS_FluorLine),
-                'binding_e': (shell_dict, xraylib.EdgeEnergy),
-                'jump': (shell_dict, xraylib.JumpFactor),
-                'yield': (shell_dict, xraylib.FluorYield),
-                }
-
-
-_OTHER_VALUES = {'H': {'Z': 1, 'rho': .524, },
-                 'He': {'Z': 2, }}
+from element_data import XRAYLIB_MAP, OTHER_VAL
 
 
 class Element(object):
 
     def __init__(self, element, energy):
-        self._element = element
-        self._energy = energy
-        self.emission_line = _XrayLibWrap('lines', element)
-        self.cs = _XrayLibWrap('cs', element, energy)
-        self.bind_energy = _XrayLibWrap('binding_e', element)
-        self.jump_factor = _XrayLibWrap('jump', element)
-        self.f_yield = _XrayLibWrap('yield', element)
+
+        if isinstance(element, str):
+            item_val = OTHER_VAL[OTHER_VAL[:, 0] == element][0]
+        elif isinstance(element, int):
+            item_val = OTHER_VAL[element-1]
+        else:
+            raise TypeError('Please define element by '
+                            'atomic number z or element name')
+        self.name = item_val[0]
+        self.z = item_val[1]['Z']
+        self.mass = item_val[1]['mass']
+        self.density = item_val[1]['rho']
+        self._element = self.z
+
+
+        if not isinstance(energy, float and int):
+            raise TypeError('Expected a number for energy')
+            self._energy = energy
+
+        self.emission_line = _XrayLibWrap('lines', self._element)
+        self.cs = _XrayLibWrap('cs', self._element, energy)
+        self.bind_energy = _XrayLibWrap('binding_e', self._element)
+        self.jump_factor = _XrayLibWrap('jump', self._element)
+        self.f_yield = _XrayLibWrap('yield', self._element)
+
+
+    @property
+    def element(self):
+        return self._element
 
     @property
     def energy(self):
         return self._energy
 
     @energy.setter
-    def energy(self, in_val):
-        self._energy = in_val
-        #self.xrf.energy = in_val
-        #self.levels.energy = in_val
+    def energy(self, val):
+        if not isinstance(val, float and int):
+            raise TypeError('Expected a number for energy')
+        self._energy = val
+        self.cs.energy = val
 
 
 class _XrayLibWrap(Mapping):
+    """
+    This is an interface to wrap xraylib to obtain calculations related
+    to xray fluorescence.
 
+    Attributes
+    ----------
+    info_type : string
+        defines which physics quantity to calculate
+    element : int
+        atomic number
+    energy : float, optional
+        incident energy for fluorescence
+    """
     def __init__(self, info_type,
                  element, energy=None):
-
         self.info_type = info_type
-        self._map, self._func = _XRAYLIB_MAP[info_type]
+        self._map, self._func = XRAYLIB_MAP[info_type]
         self._keys = sorted(list(six.iterkeys(self._map)))
         self._element = element
         self._energy = energy
@@ -77,11 +76,24 @@ class _XrayLibWrap(Mapping):
         return self._energy
 
     @energy.setter
-    def energy(self, in_val):
-        # optional sanity checks?
-        self._energy = in_val
+    def energy(self, val):
+        """
+        Parameters
+        ----------
+        val : float
+            new energy value
+        """
+        self._energy = val
 
     def __getitem__(self, key):
+        """
+        call xraylib function to calculate physics quantity
+
+        Parameters
+        ----------
+        key : string
+            defines which physics quantity to calculate
+        """
         if self.info_type == 'cs':
             return self._func(self._element,
                               self._map[key],
@@ -97,7 +109,8 @@ class _XrayLibWrap(Mapping):
         return len(self._keys)
 
 
-e = Element(30, 10)
+e = Element('Zn', 10)
+#e.energy='A'
 print e.emission_line['Ka1']
 print e.cs['Ka1']
 print e.f_yield['K']
