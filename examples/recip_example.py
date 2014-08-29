bcc966a528fbc7e9184e539d90d318abff5382ad
Sameera Abeykoon <sameera@bnl.gov>
2014-08-29 13:10:06 -0400

    WIP: modified ../examples/recip_example.py

--- examples/recip_example.py
+++ examples/recip_example.py
@@ -40,12 +40,30 @@ import nsls2.recip as recip
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+spec_folder_path = "/Volumes/Data/BeamLines/Demos/X1Data/"
+numpy_path = spec_folder_path + "LSCO_Oct13_numpy/"
+broker_path = spec_folder_path + "LSCO_Oct13_broker/"
+
+motors = np.load(broker_path+"motors.npy")
+ub = np.load(broker_path+"ub.npy")
+wavelength = np.load(broker_path+"wavelength.npy")
+temp = np.load(broker_path+"temp.npy")
+i_stack = np.load(broker_path+"i_stack.npy")
+
+scan_nos = [397,450]
+
+def reading_data():
+    pass
+
+
 def recip_ex():
     detector_size = (256, 256)
     pixel_size = (0.0135*8, 0.0135*8) # (mm)
     calibrated_center = (256/2.0, 256/2.0) # (mm)
     dist_sample = 355.0 # (mm)
-    wavelength = 13.27171266  # (Angstrom )
+    
+    
+    """wavelength = 13.27171266  # (Angstrom )
 
     # UB matrix (orientation matrix)
     ub_mat = np.array([[-1.397723053000000104e-01, -1.655595646000000087e+00, -1.405017159000000043e-02],
@@ -54,33 +72,41 @@ def recip_ex():
 
     # six angles of the diffractometer in following order
     # delta, theta, chi, phi, mu, gamma (in degrees)
-    setting_angles = np.loadtxt("setAngles.dat", unpack = True).T
+    setting_angles = np.loadtxt("setAngles.dat", unpack = True).T"""
+    for i in range(len(scan_nos)):
+        ub_mat = ub[i]
+        setting_angles = motors[i]
+        wave_length = wavelength[i]
+        I_stack = i_stack[i]
+        temperature = temp[i]
+        
+        tot_set = recip.process_to_q(setting_angles, detector_size,
+                                     pixel_size, calibrated_center,
+                                     dist_sample, wave_length, ub_mat)
+                                     
+        print (tot_set)
+        print ("#######")
 
-    tot_set = recip.process_to_q(setting_angles, detector_size,
-                                 pixel_size, calibrated_center,
-                                dist_sample, wavelength, ub_mat)
+        # make and ravel the image data (which is all ones)
+        #I = 100 * np.ones((settingAngles.shape[0], 256, 256)).ravel()
 
-    # make and ravel the image data (which is all ones)
-    I = 100 * np.ones((settingAngles.shape[0], 256, 256)).ravel()
+        # minimum and maximum values of the voxel
+        q_max = np.array([-0.200, 0.002, 1.410])
+        q_min = np.array([-0.270, -0.002, 1.370])
+        # no. of bins
+        dqn = np.array([40,40,40])
 
-    # minimum and maximum values of the voxel
-    q_max = np.array([-0.200, 0.002, 1.410])
-    q_min = np.array([-0.270, -0.002, 1.370])
-    # no. of bins
-    dqn = np.array([40,40,40])
-
-    (grid_data, grid_occu,
-         grid_std, grid_out) = recip.process_grid(tot_set, I,
-                                                  q_min, q_max,
-                                                  dqn=dqn)
-    run_plot(grid_occu)
+        (grid_data, grid_occu, grid_std, grid_out) = recip.process_grid(tot_set, I_stack.ravel(),
+                                                                        q_min, q_max, dqn=dqn)
+        print (grid_data)
+        run_plot(grid_occu)
     return
 
 
 def run_plot(grid_occu):
-    figure()
-    imshow(gridOccu.sum(0))
-    show()
+    plt.figure()
+    plt.imshow(grid_occu.sum(0))
+    plt.show()
 
 if __name__ == "__main__":
     recip_ex()
