ef2a622e053ba7fb82b5791835d5f39d1777b111
Sameera Abeykoon <sameera@bnl.gov>
2014-09-03 00:05:46 -0400

    ENH : added examples/LSCO_Nov12_broker folder with numpy array data for the recip_example.py

--- examples/recip_example.py
+++ examples/recip_example.py
@@ -42,33 +42,26 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from collections import OrderedDict
 import numpy.ma as ma
-from pylab import *
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 
 
 def recip_ex():
     detector_size = (256, 256)
-    pixel_size = (0.0135*8, 0.0135*8) # (mm)
-    calibrated_center = (256/2.0, 256/2.0) # (mm)
-    dist_sample = 355.0 # (mm)
+    pixel_size = (0.0135*8, 0.0135*8)  # (mm)
+    calibrated_center = (256/2.0, 256/2.0)  # (mm)
+    dist_sample = 355.0  # (mm)
 
-    spec_folder_path = "/Volumes/Data/BeamLines/Demos/X1Data/"
-    numpy_path = spec_folder_path + "LSCO_Oct13_numpy/"
-    broker_path = spec_folder_path + "LSCO_Oct13_broker/"
-
+    # six angles data
     motors = np.load(broker_path+"motors.npy")
+    # ub matrix data
     ub = np.load(broker_path+"ub.npy")
+    # wavelength data
     wavelength = np.load(broker_path+"wavelength.npy")
+    # temperature data
     temp = np.load(broker_path+"temp.npy")
+    # intensity of the image stack data
     i_stack = np.load(broker_path+"i_stack.npy")
-
-    scan_nos = [[56]]
-    scan_nos = OrderedDict()
-    scan_nos['E=933eV T=015K H=[-0.270,-0.200] K=[+0.002,-0.002] L=[+1.370,+1.410]'] = [[56]] # scan 56
-    for idx, tit in enumerate(scan_nos.keys()[0:2]):
-        H_range = [float(tit[18:24]) - 0.006, float(tit[25:31]) + 0.006]
-        K_range = [float(tit[36:42]) - 0.015, float(tit[43:49]) + 0.015]
-        L_range = [float(tit[54:60]) - 0.020, float(tit[61:67]) + 0.020]
-
 
     for i in range(len(scan_nos)):
         ub_mat = ub[i]
@@ -76,190 +69,116 @@ def recip_ex():
         wave_length = wavelength[i]
         I_stack = i_stack[i]
         temperature = temp[i]
-        allip = []
+
         tot_set = recip.process_to_q(setting_angles, detector_size,
                                      pixel_size, calibrated_center,
                                      dist_sample, wave_length, ub_mat)
 
-        # print (tot_set)
-        # print ("#######")
-
-        # X = tot_set[:,0].ravel()
-        # Y = tot_set[:,1].ravel()
-        # Z = tot_set[:,2].ravel()
-
+        # minimum and maximum values of the voxel
         q_min = np.array([H_range[0], K_range[0], L_range[0]])
         q_max = np.array([H_range[1], K_range[1], L_range[1]])
-        # dqn = np.array([size, size, size])
 
-
-
-        # minimum and maximum values of the voxel
-        # q_max = np.array([max(H_range), max(K_range), max(L_range)])
-        # q_min = np.array([min(H_range), min(K_range), min(L_range)])
         # no. of bins
-        dqn = np.array([40,40,1])
+        dqn = np.array([40, 40, 1])
 
         (grid_data, grid_occu, grid_std,
          grid_out) = recip.process_grid(tot_set, I_stack.ravel(), dqn=dqn)
 
-        X, Y, Z = get_grid_mesh(q_min, q_max, dqn)
+        grid = np.mgrid[0:dqn[0], 0:dqn[1], 0:dqn[2]]
+        r = (q_max - q_min) / dqn
 
-        # getGrid(tot_set,grid_data, grid_std, grid_occu)
-        # grid1 = grid_data.ravel()
-        # grid2 = grid_occu.ravel()
-        # grid3 = grid_std.ravel()
+        X = grid[0] * r[0] + q_min[0]
+        Y = grid[1] * r[1] + q_min[1]
+        Z = grid[2] * r[2] + q_min[2]
+
+        # creating a mask
         _mask = grid_occu <= 10
-        grid_Std = ma.masked_array(grid_std,_mask)
-        grid_Data = ma.masked_array(grid_data,_mask)
-        grid_Occu = ma.masked_array(grid_occu,_mask)
-        # grid1 = grid_Data.ravel()
-        # grid2 = grid_occu.ravel()
-        # grid3 = grid_Std.ravel()
-        # allip.append(X.ravel())
-        # allip.append(Y.ravel())
-        # allip.append(Z.ravel())
-        # allip.append(grid1.ravel())
-        # allip.append(grid3.ravel())
-        # allip.append(grid2.ravel())
-        # I_slice = grid_data
-        # grid = allip[0]
-        #run_plot(grid_Data, grid_Occu)
-    return X, Y, Z, grid_Data, grid_Std, grid_occu
+        grid_Std = ma.masked_array(grid_std, _mask)
+        grid_Data = ma.masked_array(grid_data, _mask)
+        grid_Occu = ma.masked_array(grid_occu, _mask)
+
+    return X, Y, Z, grid_Data, grid_Std, grid_Occu
 
 
-def get_grid_mesh(Qmin, Qmax, dQN):
-    """
+def plottdep(ip, plane='HK'):
+    grid = ip[0]
 
-    This function returns the X, Y and Z coordinates of the grid as 3d
-    arrays. (Return the grid vectors as a mesh.
-    Parameters :
-    -----------
-    Qmin : ndarray
-        minimum values of the cuboid [Qx, Qy, Qz]_min
+    x, y, i_slice, lx, x_ran, y_ran, x_tit, y_tit = get_xyz(grid, plane)
 
-    Qmax : ndarray
-        maximum values of the cuboid [Qx, Qy, Qz]_max
+    i_slice_range = [0.006, 0.0115]
 
-    dQN  : ndarray
-        No. of grid parts (bins) [Nqx, Nqy, Nqz]
-
-    Returns :
-    --------
-    X : array
-        X co-ordinate of the grid
-
-    Y : array
-        Y co-ordinate of the grid
-
-    Z : array
-        Z co-ordinate of the grid
-
-
-    Example :
-    ---------
-    These values can be used for obtaining the coordinates of each voxel.
-    For instance, the position of the (0,0,0) voxel is given by
-
-        x = X[0,0,0]
-        y = Y[0,0,0]
-        z = Z[0,0,0]
-
-    """
-    #h, k, l = Qmin + np.array((i, j, k)) * (QMax - Qmin) / dQN)
-
-    grid = np.mgrid[0:dQN[0], 0:dQN[1], 0:dQN[2]]
-    r = (Qmax - Qmin) / dQN
-
-    X = grid[0] * r[0] + Qmin[0]
-    Y = grid[1] * r[1] + Qmin[1]
-    Z = grid[2] * r[2] + Qmin[2]
-
-    return X, Y, Z
-
-
-def plottdep(ips, tit, idx = 0, plane = 'HK'):
-    grid = ips[0]
-
-    x, y, I_Slice, lx, I_Cut, xran, yran, xtit, ytit,\
-    slice_tit, cut_tit = get_xyz(grid, plane)
-
-
-    I_Slice_range = [0.006, 0.0115]
-
-    f = figure(1, figsize=(7.5, 2.3))
-    subplots_adjust(left=0.10, bottom=0.155555, right=1.05, top=0.95,
-                    wspace=0.2, hspace=0.45)
+    f = plt.figure(1, figsize=(7.5, 2.3))
+    plt.subplots_adjust(left=0.10, bottom=0.155555,
+                        right=1.05, top=0.95,
+                        wspace=0.2, hspace=0.45)
     subp = f.add_subplot(111)
 
-    cnt = subp.contourf( x, y, I_Slice, linspace(I_Slice_range[0], I_Slice_range[1],
-                                                 50, endpoint=True), extend='both')
-
+    cnt = subp.contourf(x, y, i_slice, np.linspace(i_slice_range[0],
+                                                   i_slice_range[1],
+                                                   50, endpoint=True),
+                        extend='both')
     subp.axis('scaled')
-    subp.set_xlim(xran)
-    subp.set_ylim(yran)
+    subp.set_xlim(x_ran)
+    subp.set_ylim(y_ran)
 
-    subp.set_xlabel(xtit, size=10)
-    subp.set_ylabel(ytit, size=10)
+    subp.set_xlabel(x_tit, size=10)
+    subp.set_ylabel(y_tit, size=10)
 
-    subp.tick_params( labelsize=9 )
+    subp.tick_params(labelsize=9)
 
     subp.xaxis.set_major_locator(MaxNLocator(4))
     subp.yaxis.set_major_locator(MaxNLocator(3))
 
-    cbar = colorbar(cnt, ticks=linspace(I_Slice_range[0], I_Slice_range[1],
-                                        3, endpoint=True), format='%.4f')
+    cbar = plt.colorbar(cnt, ticks=np.linspace(i_slice_range[0],
+                                                 i_slice_range[1],
+                                                 3, endpoint=True),
+                        format='%.4f')
     cbar.ax.tick_params(labelsize=8)
 
 
 def get_xyz(grid, plane):
     HKL = 'HKL'
-    for i in plane: HKL = HKL.replace(i, '')
+    for i in plane:
+        HKL = HKL.replace(i, '')
 
-    HH = grid[0][:,:,:].squeeze(); H = grid[0][:,0,0]
-    KK = grid[1][:,:,:].squeeze(); K = grid[1][0,:,0]
-    LL = grid[2][:,:,:].squeeze(); L = grid[2][0,0,:]
+    HH = grid[0][:, :, :].squeeze()
+    H = grid[0][:, 0, 0]
+    KK = grid[1][:, :, :].squeeze()
+    K = grid[1][0, :, 0]
+    LL = grid[2][:, :, :].squeeze()
+    L = grid[2][0, 0, :]
 
-    I_Slice = grid[3][:,:,:].squeeze()
-    I_Cut = I_Slice.mean( int('HKL'.find(plane[0]) < 'HKL'.find(plane[1])) )
-    print ("I_Slice")
-    print (I_Slice)
-    print ("#######")
+    i_slice = grid[3][:, :, :].squeeze()  # intensity slice
+    x_ran = eval(plane[0] + '_range')  # x range
+    y_ran = eval(plane[1] + '_range')  # y range
 
-    xran = eval(plane[0] + '_range')
-    yran = eval(plane[1] + '_range')
+    x_tit = plane[0:1]
+    y_tit = plane[1:2]
 
-    xtit = plane[0:1]; ytit = plane[1:2]
-    slice_tit =  ' %s=[%s,%s]' % (HKL, eval(HKL + '_range')[0], eval(HKL + '_range')[1])
-    cut_tit = slice_tit + (' %s=%s' % (plane[1], eval(plane[1] + '_range') ) )
-
-    x  = eval(plane[0]+plane[0])
-    y  = eval(plane[1]+plane[1])
+    x = eval(plane[0] + plane[0])
+    y = eval(plane[1] + plane[1])
     lx = eval(plane[0])
 
-    return x, y, I_Slice, lx, I_Cut, xran, yran, xtit, ytit, slice_tit, cut_tit
-
-
-def run_plot(grid_data,grid_occu):
-    plt.figure()
-    #plt.imshow(grid_data.sum(2))
-    plt.imshow(grid_occu.sum(2))
-    plt.show()
+    return x, y, i_slice, lx, x_ran, y_ran, x_tit, y_tit
 
 
 if __name__ == "__main__":
+    #  Data folder path
+    broker_path = "/Volumes/Data/All_NSLS2/NSLS2/examples/LSCO_Nov12_broker/"
+
+    # scan numbers
+    scan_nos = OrderedDict()
+    scan_nos["T = 015K"] = [[56]]
+    scanKeys = scan_nos.keys()[0:]
+    for idx, tit in enumerate(scanKeys):
+        scan_no = scan_nos[tit]
+
+    H_range = [-0.270, -0.200]
+    K_range = [+0.010, -0.010]
+    L_range = [+1.370, +1.410]
+
     ip = []
     ip.append(recip_ex())
 
-    scan_nos = [[56]]
-    scan_nos = OrderedDict()
-    scan_nos['E=933eV T=015K H=[-0.270,-0.200] K=[+0.002,-0.002] L=[+1.370,+1.410]'] = [[56]] # scan 56
-    for idx, tit in enumerate(scan_nos.keys()[0:2]):
-        H_range = [float(tit[18:24]) - 0.006, float(tit[25:31]) + 0.006]
-        K_range = [float(tit[36:42]) - 0.015, float(tit[43:49]) + 0.015]
-        L_range = [float(tit[54:60]) - 0.020, float(tit[61:67]) + 0.020]
-
-    plottdep(ip, tit, idx = 0, plane = 'HK')
-    show()
-
-    #X, Y, Z, grid1, grid2,grid3 = recip_ex()
+    plottdep(ip, plane='HK')
+    plt.show()
