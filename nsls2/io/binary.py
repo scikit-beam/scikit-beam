79383d4a4c77a9dee281ed6dd4bc9c8f710add65
Eric Dill <edill@bnl.gov>
2014-09-07 23:50:02 -0400

    DOC: numpydoc formatting

--- nsls2/io/binary.py
+++ nsls2/io/binary.py
@@ -47,30 +47,29 @@ def read_binary(filename, nx, ny, nz, dsize, headersize, **kwargs):
 
     Parameters
     ----------
-    filename: String
+    filename : String
               The name of the file to open
-    nx: integer
+    nx : integer
         The number of data elements in the x-direction
-    ny: integer
+    ny : integer
         The number of data elements in the y-direction
-    nz: integer
+    nz : integer
         The number of data elements in the z-direction
-    dsize: numpy data type
+    dsize : numpy.dtype
            The size of each element in the numpy array
-    headersize: integer
+    headersize : integer
                 The size of the file header in bytes
-    extras: dict
+    extras : dict
             unnecessary keys that were passed to this function through
             dictionary unpacking
 
     Returns
     -------
-    (data, header)
-    data: ndarray
+    data : ndarray
             data.shape = (x, y, z) if z > 1
             data.shape = (x, y) if z == 1
             data.shape = (x,) if y == 1 && z == 1
-    header: String
+    header : String
             header = file.read(headersize)
     """
 
@@ -81,7 +80,7 @@ def read_binary(filename, nx, ny, nz, dsize, headersize, **kwargs):
     header = opened_file.read(headersize)
 
     # read the entire file in as 1D list
-    data = np.fromfile(opened_file, dsize, -1)
+    data = np.fromfile(file=opened_file, dtype=dsize, count=-1)
 
     # reshape the array to 3D
     if nz is not 1:
