98f56ad9e673217f61f83e5b9bf56467b838e847
Thomas A Caswell <tcaswell@bnl.gov>
2014-09-08 23:49:38 -0400

    API : moved fit_quad_to_peak -> fitting/__init__.py
    This is a fitting (albeit a simple one) function and should
    not be hidden in the spectroscopy module.

---
+++ doc/api_changes/2014-09-08_fit_quad_to_peak.rst
@@ -0,0 +1,5 @@
+moved fit_quad_to_peak from spectroscopy.py -> fitting/__init__.py
+------------------------------------------------------------------
+
+Moved fitting function from spectroscopy to the top-level of the
+fitting module.
--- nsls2/tests/test_fitting.py
+++ nsls2/tests/test_fitting.py
@@ -2,9 +2,6 @@
 # Copyright (c) 2014, Brookhaven Science Associates, Brookhaven        #
 # National Laboratory. All rights reserved.                            #
 #                                                                      #
-# @author: Li Li (lili@bnl.gov)                                        #
-# created on 08/06/2014                                                #
-#                                                                      #
 # Redistribution and use in source and binary forms, with or without   #
 # modification, are permitted provided that the following conditions   #
 # are met:                                                             #
@@ -35,56 +32,14 @@
 # IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE   #
 # POSSIBILITY OF SUCH DAMAGE.                                          #
 ########################################################################
-
-
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import six
 
-import logging
-logger = logging.getLogger(__name__)
-import numpy as np
+from nsls2.testing.decorators import known_fail_if
 
 
-def fit_quad_to_peak(x, y):
-    """
-    Fits a quadratic to the data points handed in
-    to the from y = b[0](x-b[1])**2 + b[2] and R2
-    (measure of goodness of fit)
-
-    Parameters
-    ----------
-    x : ndarray
-        locations
-    y : ndarray
-        values
-
-    Returns
-    -------
-    b : tuple
-       coefficients of form y = b[0](x-b[1])**2 + b[2]
-
-    R2 : float
-      R2 value
-
-    """
-
-    lenx = len(x)
-
-    # some sanity checks
-    if lenx < 3:
-        raise Exception('insufficient points handed in ')
-    # set up fitting array
-    X = np.vstack((x ** 2, x, np.ones(lenx))).T
-    # use linear least squares fitting
-    beta, _, _, _ = np.linalg.lstsq(X, y)
-
-    SSerr = np.sum((np.polyval(beta, x) - y)**2)
-    SStot = np.sum((y - np.mean(y))**2)
-    # re-map the returned value to match the form we want
-    ret_beta = (beta[0],
-                -beta[1] / (2 * beta[0]),
-                beta[2] - beta[0] * (beta[1] / (2 * beta[0])) ** 2)
-
-    return ret_beta, 1 - SSerr / SStot
+@known_fail_if(True)
+def test_fit_quad_to_peak():
+    assert(True)
