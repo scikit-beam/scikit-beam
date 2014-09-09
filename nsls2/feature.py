508e3291e3b7ba443c6ef7952c07088457ced1e7
Thomas A Caswell <tcaswell@bnl.gov>
2014-09-09 00:22:21 -0400

    ENH : added 1D peak refinement
    This is a bit of frame work to refine the locations of peaks (or
    valleys in principle) of 1D curves.  The assumption is that the
    location of the extrema has been identified to pixel(sampling)-level
    accuracy and the user has a function that will refine a single peak.
    
    This commit includes
     - peak_refinement
        - logic to loop over all the peaks
        - call refine on each peak
        - marshal the output back to the user
     - two refinement functions

---
+++ nsls2/feature.py
@@ -0,0 +1,215 @@
+# ######################################################################
+# Copyright (c) 2014, Brookhaven Science Associates, Brookhaven        #
+# National Laboratory. All rights reserved.                            #
+#                                                                      #
+# Redistribution and use in source and binary forms, with or without   #
+# modification, are permitted provided that the following conditions   #
+# are met:                                                             #
+#                                                                      #
+# * Redistributions of source code must retain the above copyright     #
+#   notice, this list of conditions and the following disclaimer.      #
+#                                                                      #
+# * Redistributions in binary form must reproduce the above copyright  #
+#   notice this list of conditions and the following disclaimer in     #
+#   the documentation and/or other materials provided with the         #
+#   distribution.                                                      #
+#                                                                      #
+# * Neither the name of the Brookhaven Science Associates, Brookhaven  #
+#   National Laboratory nor the names of its contributors may be used  #
+#   to endorse or promote products derived from this software without  #
+#   specific prior written permission.                                 #
+#                                                                      #
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS  #
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT    #
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS    #
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE       #
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,           #
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES   #
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR   #
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)   #
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,  #
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OTHERWISE) ARISING   #
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE   #
+# POSSIBILITY OF SUCH DAMAGE.                                          #
+########################################################################
+"""
+This module contains code for extracting features from data
+"""
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+import logging
+logger = logging.getLogger(__name__)
+
+import six
+from six.moves import zip
+import numpy as np
+
+from collections import deque
+
+from .fitting import fit_quad_to_peak
+
+
+class RejectPeak(Exception):
+    """
+    Custom exception class to indicate that the refine function rejected
+    the
+    """
+    pass
+
+
+def peak_refinement(x, y, cands, window, refine_function, refine_args=None):
+    """Refine candidate locations
+
+    Parameters
+    ----------
+    x : array
+        The independent variable, does not need to be evenly spaced.
+
+    y : array
+        The dependent variable
+
+    cands : array
+        Array of the indicies in x, y for candidate peaks.
+
+    refine_function : function
+        A function which takes a section of data with a peak in it and returns
+        the location and height of the peak to sub-sample accuracy.  Additional
+        parameters can be passed through via the refine_args kwarg.
+        The function signature must be::
+
+            center, height = refine_func(x, y, **kwargs)
+
+        This function may raise `RejectPeak` to indicate no suitable
+        peak was found
+
+    window : int
+        How many samples to extract on either side of the
+        candidate locations are passed to the refine function.  The
+        window will be truncated near the boundaries.  The length of the
+        data passed to the refine function will be (2 * window + 1).
+
+    refine_args : dict, optional
+    The passed to the refine_function
+
+    Returns
+    -------
+    peak_locations : array
+        The locations of the peaks
+
+    peak_heights : array
+        The heights of the peaks
+
+    Examples
+    --------
+    >>> x = np.arange(512)
+    >>> tt = np.zeros(512)
+    >>> tt += np.exp(-((x - 150.55)/10)**2)
+    >>> tt += np.exp(-((x - 450.75)/10)**2)
+    >>> cands = scipy.signal.argrelmax(tt)[0]
+
+    >>> print(peak_refinement(x, tt, cands, 10, refine_quadratic))
+    (array([ 150.62286432,  450.7909412 ]), array([ 0.96435832,  0.96491501]))
+    >>> print(peak_refinement(x, tt, cands, 10, refine_log_quadratic))
+    (array([ 150.55,  450.75]), array([ 1.,  1.]))
+    """
+    # clean up input
+    x = np.asarray(x)
+    y = np.asarray(y)
+    cands = np.asarray(cands, dtype=int)
+    window = int(window)
+    if refine_args is None:
+        refine_args = dict()
+    # local working variables
+    out_tmp = deque()
+    max_ind = len(x)
+
+    for ind in cands:
+        slc = slice(np.max([0, ind-window]),
+                    np.min([max_ind, ind + window + 1]))
+        try:
+            ret = refine_function(x[slc], y[slc], **refine_args)
+        except RejectPeak:
+            #
+            continue
+        else:
+            out_tmp.append(ret)
+
+    return tuple([np.array(_) for _ in zip(*out_tmp)])
+
+
+def refine_quadratic(x, y, Rval_thresh=None):
+    """
+    Attempts to refine the peaks by fitting to
+    a quadratic.
+
+    Parameters
+    ----------
+    x : array
+        Independent variable
+
+    y : array
+        Dependent variable
+
+    Rval_thresh : float, optional
+        Threshold for R2 value of fit,  If the computed R2 is worse than
+        this threshold RejectPeak will be raised
+
+    Returns
+    -------
+    center : float
+        Refined estimate for center
+
+    height : float
+        Refined estimate for height
+
+    Raises
+    ------
+    RejectPeak
+       Raised to indicate that no suitable peak was found in the
+       interval
+
+    """
+    beta, R2 = fit_quad_to_peak(x, y)
+    if Rval_thresh is not None:
+        if R2 < Rval_thresh:
+            raise RejectPeak()
+    return beta[1], beta[2]
+
+
+def refine_log_quadratic(x, y, Rval_thresh=None):
+    """
+    Attempts to refine the peaks by fitting to a quadratic to the log of
+    the y-data.  This is a linear approximation of fitting a Gaussian.
+
+    Parameters
+    ----------
+    x : array
+        Independent variable
+
+    y : array
+        Dependent variable
+
+    Rval_thresh : float, optional
+        Threshold for R2 value of fit,  If the computed R2 is worse than
+        this threshold RejectPeak will be raised
+
+    Returns
+    -------
+    center : float
+        Refined estimate for center
+
+    height : float
+        Refined estimate for height
+
+    Raises
+    ------
+    RejectPeak
+       Raised to indicate that no suitable peak was found in the
+       interval
+
+    """
+    beta, R2 = fit_quad_to_peak(x, np.log(y))
+    if Rval_thresh is not None:
+        if R2 < Rval_thresh:
+            raise RejectPeak()
+    return beta[1], np.exp(beta[2])
