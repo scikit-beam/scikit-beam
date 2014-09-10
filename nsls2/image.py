b5ca0008eeada07b3050b0cf49398c3f8e24b1ab
Thomas A Caswell <tcaswell@bnl.gov>
2014-09-10 18:50:43 -0400

    BUG : fixed transpose error
    Now returns the center (row, col)

--- nsls2/image.py
+++ nsls2/image.py
@@ -69,7 +69,7 @@ def find_ring_center_acorr_1D(input_image):
         are centered on.  Accurate to pixel resolution.
     """
     return tuple(bins[np.argmax(vals)] for vals, bins in
-                  (_corr_ax1(_im) for _im in (input_image, input_image.T)))
+                  (_corr_ax1(_im) for _im in (input_image.T, input_image)))
 
 
 def _corr_ax1(input_image):
