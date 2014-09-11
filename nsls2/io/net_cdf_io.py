c48d970248d4330f07c9f374efa7bdfc0f5912fd
Gabriel Iltis <giltis@bnl.gov>
2014-09-11 14:51:15 -0400

    DEV: Updated and tested the load netCDF function.
    Thus far this only includes file loading. Eventually I need to add
    the file save functionality.

--- nsls2/io/net_cdf_io.py
+++ nsls2/io/net_cdf_io.py
@@ -1,6 +1,6 @@
 # Module for the BNL image processing project
 # Developed at the NSLS-II, Brookhaven National Laboratory
-# Developed by Gabriel Iltis, Nov. 2013
+# Developed by Gabriel Iltis, Sept. 2014
 """
 This module contains fileIO operations and file conversion for the image 
 processing tool kit in the NSLS-II data analysis software package. 
@@ -23,8 +23,7 @@ import numpy as np
 from netCDF4 import Dataset
 
 
-def load_netCDF(file_name, 
-                data_type = None):
+def load_netCDF(file_name):
     """
     This function loads the specified netCDF file format data set (e.g.*.volume 
     APS-Sector 13 GSECARS extension) file into a numpy array for further analysis.
@@ -59,51 +58,46 @@ def load_netCDF(file_name,
     file_name : string
         Complete path to the file to be loaded into memory
     
-    data_type : integer
-        Z-axis array dimension as an integer value
 
-    
     Returns
     -------
+    md_dict : dict
+        Dictionary containing all metadata contained in the netCDF file.
+        This metadata contains data collection, and experiment information
+        as well as values and variables pertinent to the image data.
+
     data : ndarray
         ndarray containing the image data contained in the netCDF file.
         The image data is scaled using the scale factor defined in the
         netCDF metadata, if a scale factor was recorded during data 
         acquisition or reconstruction. If a scale factor is not present, 
         then a default value of 1.0 is used.
-    
-    md_dict : dict
-        Dictionary containing all metadata contained in the netCDF file.
-        This metadata contains data collection, and experiment information
-        as well as values and variables pertinent to the image data.
     """
     
 
     src_file = Dataset(file_name)
     data = src_file.variables['VOLUME']
     md_dict = src_file.__dict__
-    try:
+    if data.scale_factor != 1.0:
         #Check for voxel intensity scale factor and apply if value is present
         scale_value = data.scale_factor
-    except:
+    else:
         # Value is set to 1.0 otherwise so values are not altered, other than 
         # to ensure values are of type float
         scale_value = 1.0
-    data=data/scale_value
+    data = data / scale_value
 
-    try:
-        # Accounts for specific case where z_pixel_size doesn't get assigned 
-        # even though dimensions are actuall isotropic. This occurs when 
-        # reconstruction is completed using tomo_recon on data collected at APS-13BMD.
-        if md_dict['x_pixel_size'] == md_dict['y_pixel_size'] 
-            and 
-            md_dict['z_pixel_size'] == 0.0 
-            and 
-            data.shape[0] > 1:
-                md_dict['voxel_size'] = {
-                        'value' : md_dict['x_pixel_size'],
-                        'type' : float,
-                        'units' : ''
-                        }
-    return data, md_dict 
+    # Accounts for specific case where z_pixel_size doesn't get assigned
+    # even though dimensions are actuall isotropic. This occurs when
+    # reconstruction is completed using tomo_recon on data collected at
+    # APS-13BMD.
+    if (md_dict['x_pixel_size'] == md_dict['y_pixel_size'] and
+                md_dict['z_pixel_size'] == 0.0 and
+                data.shape[0] > 1):
+        md_dict['voxel_size'] = {
+            'value' : md_dict['x_pixel_size'],
+            'type' : float,
+            'units' : ''
+            }
+    return md_dict, data
 
