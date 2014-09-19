39e14daf47e507dd42f9d0ac1863d0efa33fc0fc
Gabriel Iltis <giltis@bnl.gov>
2014-09-19 12:34:40 -0400

    DEV: scipy filter functions cnvrt to direct import and update docs

--- nsls2/img_proc/filterops.py
+++ nsls2/img_proc/filterops.py
@@ -15,32 +15,44 @@ GCI: 2/19/2014 -- Changed file name from C5_filterops.py to filterops.py
       Updated documentation to docstring format.
       Converted back from class structure to basic module containing function 
       definitions.
+GCI: 9/19/14 -- Scipy filtering tools converted to direct imports,
+and documentation for the filtering functions is reassigned to the
+documentation I originally wrote for the functions. This doc conversion is
+necessary in order to fit the autowrap format conventions required for
+incorporation into VisTrails
 """
 
-import scipy.ndimage.filters as fltr
+#import scipy.ndimage.filters as fltr
 
-
-def gauss_fltr(src_data, 
-               sigma, 
-               order):
+from scipy.ndimage.filters import gaussian_filter
+from scipy.ndimage.filters import median_filter
+from scipy.ndimage.filters import minimum_filter
+from scipy.ndimage.filters import maximum_filter
+from scipy.ndimage.filters import gaussian_gradient_magnitude
+from scipy.ndimage.filters import gaussian_laplace
+from scipy.ndimage.filters import laplace
+from scipy.ndimage.filters import percentile_filter
+from scipy.ndimage.filters import sobel
+#
+gaussian_filter.__doc__ = (
     """
-    This function applies a multi-dimensional gaussian filter to the source 
+    This function applies a multi-dimensional gaussian filter to the source
     data set.
 
     Parameters
     ----------
-    src_data : numpy array
+    src_data : ndarray
         Specify the volume to be filtered
-    
-    sigma : int or int containing tuple
-        This value specifies the standard deviation for the gaussian filter and 
-        can be entered as a single value, or for anisotropic filtering this 
-        parameter can contain a sequence of values associated with each axis 
+
+    sigma : tuple
+        This value specifies the standard deviation for the gaussian filter and
+        can be entered as a single value, or for anisotropic filtering this
+        parameter can contain a sequence of values associated with each axis
         of the source data.
-    
+
     order : int
-        This value specifies the derivative order of the gaussian kernel which 
-        will be used to convolve the source data. Four options are available, 
+        This value specifies the derivative order of the gaussian kernel which
+        will be used to convolve the source data. Four options are available,
         including:
             0 -- Convolution with a Gaussian kernel
             1 -- Convolution with the first derivative of the Gaussian kernel
@@ -49,233 +61,209 @@ def gauss_fltr(src_data,
 
     Returns
     -------
-    output : numpy array
-        The function returns the array containing the filtered result to the 
+    output : ndarray
+        The function returns the array containing the filtered result to the
         specified variable.
     """
-    output = fltr.gaussian_filter(src_data, 
-                                  sigma, 
-                                  order)
-    return output
+    )
 
 
-def median_fltr(src_data, 
-                kernel_size):
-    """
-    This function applies a multi-dimensional median filter to the source data 
-    set.
-    
-    Parameters
-    ----------
-    src_data : numpy array
-        Specify the volume to be filtered
-    
-    kernel_size : tuple of kernel dimensions
-        Specify the kernel size to be utilized in the filtering operation
-
-    Returns
-    -------
-    output : numpy array
-        The function returns the array containing the filtered result to the 
-        specified variable.
-    """
-    output = fltr.median_filter(src_data, 
-                                size=kernel_size)
-    return output
-
-
-def min_fltr(src_data, 
-             kernel_size):
-    """
-    This function applies a multi-dimensional minimum filter to the source data 
+median_filter.__doc__ = (
+     """
+    This function applies a multi-dimensional median filter to the source data
     set.
 
     Parameters
     ----------
-    src_data : numpy array
+    src_data : ndarray
         Specify the volume to be filtered
-    
-    kernel_size : tuple of kernel dimensions
+
+    kernel_size : tuple
         Specify the kernel size to be utilized in the filtering operation
 
     Returns
     -------
-    output : numpy array
-        The function returns the array containing the filtered result to the 
+    output : ndarray
+        The function returns the array containing the filtered result to the
         specified variable.
     """
-    output = fltr.minimum_filter(src_data, 
-                                 size=kernel_size)
-    return output
+    )
 
-
-def max_fltr(src_data, 
-             kernel_size):
+minimum_filter.__doc__ = (
     """
-    This function applies a multi-dimensional maximum filter to the source data 
+    This function applies a multi-dimensional minimum filter to the source data
     set.
-    
+
     Parameters
     ----------
-    src_data : numpy array
+    src_data : ndarray
         Specify the volume to be filtered
-    
-    kernel_size : tuple of kernel dimensions
+
+    kernel_size : tuple
         Specify the kernel size to be utilized in the filtering operation
 
     Returns
     -------
-    output : numpy array
-        The function returns the array containing the filtered result to the 
+    output : ndarray
+        The function returns the array containing the filtered result to the
         specified variable.
     """
-    output = fltr.maximum_filter(src_data, 
-                                 size=kernel_size)
-    return output
+    )
 
 
-def gradMag_gauss_fltr (src_data, 
-                        sigma):
+maximum_filter.__doc__ = (
     """
-    This function applies a multi-dimensional gradient magnitude filter using 
+    This function applies a multi-dimensional maximum filter to the source data
+    set.
+
+    Parameters
+    ----------
+    src_data : ndarray
+        Specify the volume to be filtered
+
+    kernel_size : tuple
+        Specify the kernel size to be utilized in the filtering operation
+
+    Returns
+    -------
+    output : ndarray
+        The function returns the array containing the filtered result to the
+        specified variable.
+    """
+    )
+
+gaussian_gradient_magnitude.__doc__ = (
+    """
+    This function applies a multi-dimensional gradient magnitude filter using
     Gaussian derivatives on the source data set.
-    
+
     Parameters
     ----------
-    src_data : numpy array
+    src_data : ndarray
         Specify the volume to be filtered
-    
-    sigma : int or int tuple
-        This value specifies the standard deviation for the gaussian filter and 
-        can be entered as a single value, or for anisotropic filtering this 
-        parameter can contain a sequence of values associated with each axis of 
+
+    sigma : tuple
+        This value specifies the standard deviation for the gaussian filter and
+        can be entered as a single value, or for anisotropic filtering this
+        parameter can contain a sequence of values associated with each axis of
         the source data.
 
     Returns
     -------
-    output : numpy array
-        The function returns the array containing the filtered result to the 
+    output : ndarray
+        The function returns the array containing the filtered result to the
         specified variable.
     """
-    output = fltr.gaussian_gradient_magnitude(src_data, 
-                                              sigma)
-    return output
+    )
 
 
-def laplace_gauss_fltr (src_data, 
-                        sigma):
+gaussian_laplace.__doc__ = (
     """
-    This function applies a multi-dimensional Laplace filter using Gaussian 
+    This function applies a multi-dimensional Laplace filter using Gaussian
     second derivatives on the source data set.
-    
+
     Parameters
     ----------
-    src_data : numpy array
+    src_data : ndarray
         Specify the volume to be filtered
-    
+
     sigma : int
-        This value specifies the standard deviation for the gaussian filter and 
-        can be entered as a single value, or for anisotropic filtering this 
-        parameter can contain a sequence of values associated with each axis of 
+        This value specifies the standard deviation for the gaussian filter and
+        can be entered as a single value, or for anisotropic filtering this
+        parameter can contain a sequence of values associated with each axis of
         the source data.
 
     Returns
     -------
-    output : numpy array
-        The function returns the array containing the filtered result to the 
+    output : ndarray
+        The function returns the array containing the filtered result to the
         specified variable.
     """
-    output = fltr.gaussian_laplace(src_data, 
-                                   sigma)
-    return output
+    )
 
 
-def laplace_fltr (src_data):
+laplace.__doc__ = (
     """
-    This function applies a multi-dimensional Laplace filter using approximate 
+    This function applies a multi-dimensional Laplace filter using approximate
     second derivatives of the source data set.
-    
+
     Parameters
     ----------
-    src_data : numpy array
+    src_data : ndarray
         Specify the volume to be filtered
-    
+
     Returns
     -------
-    output : numpy array
-        The function returns the array containing the filtered result to the 
+    output : ndarray
+        The function returns the array containing the filtered result to the
         specified variable.
     """
-    output = fltr.laplace(src_data)
-    return output
+    )
 
 
-def percent_fltr (src_data, 
-                  percentile, 
-                  kernel_size):
+percentile_filter.__doc__ = (
     """
-    This function applies a multi-dimensional percentile filter to the source 
-    data set. The percentile filter replaces the value of the central voxel with 
-    a percentile of all voxels included in the kernel. 
-    NOTE: that application of the percentile filter using the 50th percentile 
+    This function applies a multi-dimensional percentile filter to the source
+    data set. The percentile filter replaces the value of the central voxel with
+    a percentile of all voxels included in the kernel.
+    NOTE: that application of the percentile filter using the 50th percentile
     (percentile = 50) is equivalent to the median filter.
-    
+
     Parameters
     ----------
-    src_data : numpy array
+    src_data : ndarray
         Specify the volume to be filtered
-    
+
     percentile : int
-        Specify percentile value (range of -99 to 100) 
-    
-    kernel_size : int tuple
+        Specify percentile value (range of -99 to 100)
+
+    kernel_size : tuple
         Specify the kernel size to be utilized in the filtering operation
 
     Returns
     -------
-    output : numpy array
-        The function returns the array containing the filtered result to the 
+    output : ndarray
+        The function returns the array containing the filtered result to the
         specified variable.
     """
-    output = fltr.percentile_filter(src_data, 
-                                    percentile, 
-                                    size=kernel_size)
-    return output
+    )
 
 
-def sobel_fltr (src_data):
+sobel.__doc__ = (
     """
-    This function computes a basic Sobel filter on the source data set. The 
+    This function computes a basic Sobel filter on the source data set. The
     Sobel filter can be used to aid in edge, and boundary region detection.
-    
+
     Parameters
     ----------
-    src_data : numpy array
+    src_data : ndarray
         Specify the volume to be filtered
-    
+
     Returns
     -------
-    output : numpy array
-        The function returns the array containing the filtered result to the 
+    output : ndarray
+        The function returns the array containing the filtered result to the
         specified variable.
     """
-    output = fltr.sobel(src_data)
-    return output
-
-#def TV_fltr():
+    )
 
 
-#def NLmeans():
+def TV_fltr():
+    pass
 
 
-def fltr_obj_dict_create (fltr_mod_name):
-    filter_ops_dict = {"kernel based" : {"median" : fltr_mod_name.median_fltr, "minimum" : fltr_mod_name.min_fltr, "maximum" : fltr_mod_name.max_fltr},
-                       "percentile" : fltr_mod_name.percent_fltr,
-                       "gaussian" : fltr_mod_name.gauss_fltr,
-                       "gradient magnitude: gaussian" : fltr_mod_name.gradMag_gauss_fltr,
-                       "laplace" : fltr_mod_name.laplace_fltr,
-                       "laplace: gaussian approx." : fltr_mod_name.laplace_gauss_fltr,
-                       "edge detect" : {"sobel" : fltr_mod_name.sobel_fltr}
-                       }
-    return filter_ops_dict
+def NLmeans():
+    pass
 
+
+# def fltr_obj_dict_create (fltr_mod_name):
+#     filter_ops_dict = {"kernel based" : {"median" : fltr_mod_name.median_fltr, "minimum" : fltr_mod_name.min_fltr, "maximum" : fltr_mod_name.max_fltr},
+#                        "percentile" : fltr_mod_name.percent_fltr,
+#                        "gaussian" : fltr_mod_name.gauss_fltr,
+#                        "gradient magnitude: gaussian" : fltr_mod_name.gradMag_gauss_fltr,
+#                        "laplace" : fltr_mod_name.laplace_fltr,
+#                        "laplace: gaussian approx." : fltr_mod_name.laplace_gauss_fltr,
+#                        "edge detect" : {"sobel" : fltr_mod_name.sobel_fltr}
+#                        }
+#     return filter_ops_dict
+#
