2cded4807fac945a34a12b5b2b5c6195783d31a4
Gabriel Iltis <giltis@bnl.gov>
2014-09-29 17:02:19 -0400

    DEV: Adds all image processing tools currently wrapped into vistrails.
    Current tool sets that have been incorporated include thresholding
    functions, image filters, histogram generators, morphological filters, and volume
    transformation functions.

--- nsls2/img_proc/filterops.py
+++ nsls2/img_proc/filterops.py
@@ -31,8 +31,19 @@ from scipy.ndimage.filters import gaussian_laplace
 from scipy.ndimage.filters import laplace
 from scipy.ndimage.filters import percentile_filter
 from scipy.ndimage.filters import sobel
-#
+from scipy.ndimage.filters import prewitt
+from scipy.ndimage.filters import rank_filter
 
+#Need additional work in order to implement in vistrails since the function
+#to apply needs to be defined in order to use these filters.
+# from scipy.ndimage.filters import generic_filter
+# from scipy.ndimage.filters import generic_gradient_magnitude
+# from scipy.ndimage.filters import generic_laplace
+# from scipy.ndimage.filters import uniform_filter
+# from scipy.ndimage.fourier import fourier_ellipsoid
+# from scipy.ndimage.fourier import fourier_gaussian
+# from scipy.ndimage.fourier import fourier_shift
+# from scipy.ndimage.fourier import fourier_uniform
 
 def TV_fltr():
     pass
