4d87485422a9bfb0485b6a727e2cb00d368beaf1
Sameera Abeykoon <sameera@bnl.gov>
2014-10-22 15:47:15 -0400

    WIP: Converts radius from the calibrated center to twotheta

--- nsls2/core.py
+++ nsls2/core.py
@@ -730,6 +730,25 @@ def pixel_to_phi(shape, calibrated_center, pixel_size=None):
     return np.arctan2(X, Y)
 
 
+def radius_to_twotheta(dist_sample, radius):
+    """
+    Converts radius from the calibrated center to twotheta
+
+    Parameters
+    ----------
+    dist_sample : float
+
+    radius : array
+        The L2 norm of the distance of each pixel from the calibrated center.
+
+    Returns
+    -------
+    two_theta : array
+        An array of :math:`2\\theta` values
+    """
+    return 2 * np.arctan(radius / dist_sample)
+
+
 def wedge_integration(src_data, center, theta_start,
                       delta_theta, r_inner, delta_r):
     """
