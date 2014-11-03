5cfeba8002d5148596ee4bde15327fca5cdef6b0
Gabriel Iltis <giltis@bnl.gov>
2014-11-03 15:26:14 -0500

    DEV: Filter operations have been simplified for direct loading
    Basic filters are currently included. More complicated filters
    require some additional work, and will be enabled in the near
    future.
    With the basic functions operational more complex filters such as Total
    Variation and Non-Local Means can be focused on and will be added to
    this module.

--- nsls2/img_proc/filterops.py
+++ nsls2/img_proc/filterops.py
@@ -2,24 +2,10 @@
 # Developed at the NSLS-II, Brookhaven National Laboratory
 # Developed by Gabriel Iltis, Oct. 2013
 """
-This class contains functions used for filtering images and volume data sets.
-The basic filters included in this class utilize the filter set included in
+This module contains functions used for filtering images and volume data sets.
+The basic filters included in this module utilize the filter set included in
 scipy.ndimage.filters. More advanced filtering operations will be added 
 shortly.
-"""
-"""
-REVISION LOG: (FORMAT: "PROGRAMMER INITIALS: DATE -- RECORD")
-8GCI: 2/11/2014 -- Modifying documentation of the package functions for 
-		   inclusion in the bulk module pull to GITHUB
-GCI: 2/19/2014 -- Changed file name from C5_filterops.py to filterops.py
-      Updated documentation to docstring format.
-      Converted back from class structure to basic module containing function 
-      definitions.
-GCI: 9/19/14 -- Scipy filtering tools converted to direct imports,
-and documentation for the filtering functions is reassigned to the
-documentation I originally wrote for the functions. This doc conversion is
-necessary in order to fit the autowrap format conventions required for
-incorporation into VisTrails
 """
 
 from scipy.ndimage.filters import gaussian_filter
