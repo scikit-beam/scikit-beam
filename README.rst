894a77864e642ad28ea808d6be38986bfd31ef5e
Eric Dill <edill@bnl.gov>
2014-11-12 14:56:01 -0500

    MNT: Readme converted to rst and setup.py updated

---
+++ README.rst
@@ -0,0 +1,13 @@
+.. image:: https://coveralls.io/repos/Nikea/scikit-xray/badge.png :target: https://coveralls.io/r/Nikea/scikit-xray
+
+.. image:: https://travis-ci.org/Nikea/scikit-xray.svg?branch=master
+    :target: https://travis-ci.org/Nikea/scikit-xray
+
+.. image:: https://graphs.waffle.io/Nikea/scikit-xray/throughput.svg
+ :target: https://waffle.io/Nikea/scikit-xray/metrics
+ :alt: 'Throughput Graph'
+
+scikit-xray
+=====
+
+`Documentation <http://Nikea.github.io/scikit-xray/>`_
--- setup.py
+++ setup.py
@@ -4,11 +4,20 @@ import setuptools
 from distutils.core import setup, Extension
 from setupext import ext_modules
 import numpy as np
+import os
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name='skxray',
-    version='0.0.x',
+    name='scikit-xray',
+    version='0.0.2',
     author='Brookhaven National Lab',
+    description="Data analysis tools for X-ray science",
     packages=["skxray",
               "skxray.testing",
               "skxray.fitting",
@@ -16,5 +25,20 @@ setup(
               "skxray.io",
               ],
     include_dirs=[np.get_include()],
-    ext_modules=ext_modules
+    ext_modules=ext_modules,
+    url='http://github.com/Nikea/scikit-xray',
+    long_description=read('README.md'),
+    keywords='Xray Analysis',
+    license='BSD',
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Topic :: ',
+                 "License :: OSI Approved :: BSD License",
+                 "Programming Language :: Python :: 2.7",
+                 "Programming Language :: Python :: 3.4",
+                 "Topic :: Scientific/Engineering :: Physics",
+                 "Topic :: Scientific/Engineering :: Chemistry",
+                 "Topic :: Software Development :: Libraries",
+                 "Intended Audience :: Science/Research",
+                 "Intended Audience :: Developers",
+                 ]
     )
