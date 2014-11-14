249782c8d6cdf07ac5e5535862f2e59a0138a3c8
Cheng Chang <cheng.chang@stonybrook.edu>
2014-11-14 13:38:49 -0500

    Changed file modes and line endings

--- skxray/dpc.py
+++ skxray/dpc.py
@@ -47,57 +47,57 @@ from scipy.optimize import minimize
 
 
 def image_reduction(im, roi=None, bad_pixels=None):
-    """ 
+    """
     Sum the image data along one dimension
-    
+
     Parameters
     ----------
     im : 2-D numpy array
         store the image data
-    
+
     roi : numpy.ndarray, optional
-        upper left co-ordinates of roi's and the, length and width of roi's 
+        upper left co-ordinates of roi's and the, length and width of roi's
         from those co-ordinates
-    
+
     bad_pixels : list, optional
         store the coordinates of bad pixels
         [(1, 5), (2, 6)] --> 2 bad pixels --> (1, 5) and (2, 6)
-    
+
     Returns
     ----------
     xline : 1-D numpu array
         the sum of the image data along x direction
-    
+
     yline : 1-D numpy array
         the sum of the image data along y direction
-    
+
     """
-    
+
     if bad_pixels is not None:
         for x, y in bad_pixels:
             try:
                 im[x, y] = 0
             except IndexError:
                 logging.warning("Bad pixel indexes are out of range.")
-    
+
     if roi is not None:
         x, y, w, l = roi
         try:
             im = im[x : x + w, y : y + l]
         except IndexError:
             logging.warning("The ROI is out of range.")
-    
+
     xline = np.sum(im, axis=0)
     yline = np.sum(im, axis=1)
-    
+
     return xline, yline
-    
-    
+
+
 def _rss_factory(length):
     """
     A factory function for returning a residue function for use in dpc fitting.
 
-    The main reason to do this is to generate a closure over beta so that 
+    The main reason to do this is to generate a closure over beta so that
     linspace is only called once.
 
     Parameters
@@ -108,68 +108,68 @@ def _rss_factory(length):
     Returns
     -------
     function
-        A function with signature f(v, xdata, ydata) which is suitable for use 
+        A function with signature f(v, xdata, ydata) which is suitable for use
         as a cost function for use with scipy.optimize.
-        
+
     """
-    
+
     beta = 1j * (np.linspace(-(length-1)//2, (length-1)//2, length))
-    
+
     def _rss(v, xdata, ydata):
-        """ 
+        """
         Internal function used by fit()
         Cost function to be minimized in nonlinear fitting
-        
+
         Parameters
         ----------
         v : list
             store the fitting value
             v[0], intensity attenuation
             v[1], phase gradient along x or y direction
-	
+
         xdata : 1-D complex numpy array
             auxiliary data in nonlinear fitting
             returning result of ifft1D()
-	
+
         ydata : 1-D complex numpy array
         auxiliary data in nonlinear fitting
         returning result of ifft1D()
-	
+
         Returns
         --------
         ret : float
             residue value
-	
-	    """
-			
+
+        """
+
         diff = ydata - xdata * v[0] * np.exp(v[1] * beta)
         ret = np.sum((diff * np.conj(diff)).real)
-	
+
         return ret
-	
+
     return _rss
 
 
 
 def dpc_fit(rss, ref_f, f, start_point, solver, tol=1e-6, max_iters=2000):
-    """ 
-    Nonlinear fitting for 2 points 
-    
+    """
+    Nonlinear fitting for 2 points
+
     Parameters
     ----------
     rss : function
         objective function to be minimized in DPC fitting
-        
+
     ref_f : 1-D numpy array
         One of the two arrays used for nonlinear fitting
-    
+
     f : 1-D numpy array
         One of the two arrays used for nonlinear fitting
-    
+
     start_point : 2-element list
         start_point[0], start-searching point for the intensity attenuation
         start_point[1], start-searching point for the phase gradient
-    
+
     solver : string
         type of solver, one of the following
         * 'Nelder-Mead'
@@ -184,30 +184,30 @@ def dpc_fit(rss, ref_f, f, start_point, solver, tol=1e-6, max_iters=2000):
         * 'SLSQP'
         * 'dogleg'
         * 'trust-ncg'
-    
+
     tol : float, optional
         termination criteria of nonlinear fitting
-    
+
     max_iters : integer, optional
         maximum iterations of nonlinear fitting
-    
+
     Returns:
     ----------
     a : float
         fitting result: intensity attenuation
-    
+
     g : float
         fitting result: phase gradient
-        
+
     """
-    
-    res = minimize(rss, start_point, args=(ref_f, f), method=solver, tol=tol, 
+
+    res = minimize(rss, start_point, args=(ref_f, f), method=solver, tol=tol,
                    options=dict(maxiter=max_iters))
-                    
+
     vx = res.x
     a = vx[0]
     g = vx[1]
-        
+
     return a, g
 
 # attributes
@@ -227,133 +227,133 @@ dpc_fit.solver = ['Nelder-Mead',
 
 
 def recon(gx, gy, dx, dy, padding=0, w=1.):
-    """ 
-    Reconstruct the final phase image 
-    
+    """
+    Reconstruct the final phase image
+
     Parameters
     ----------
     gx : 2-D numpy array
         phase gradient along x direction
-    
+
     gy : 2-D numpy array
         phase gradient along y direction
-    
+
     dx : float
         scanning step size in x direction (in micro-meter)
-    
+
     dy : float
         scanning step size in y direction (in micro-meter)
-    
+
     padding : integer, optional
         padding parameter
         default value, padding = 0 --> no padding
                         p p p
         padding = 1 --> p v p
                         p p p
-             
+
     w : float, optional
         weighting parameter for the phase gradient along x and y direction when
         constructing the final phase image
         default value = 0, which means that gx and gy equally contribute to
         the final phase image
-    
+
     Returns
     ----------
     phi : 2-D numpy array
         final phase image
-        
+
     References
     ----------
     [1] Yan, Hanfei, Yong S. Chu, Jorg Maser, Evgeny Nazaretski, Jungdae Kim,
     Hyon Chol Kang, Jeffrey J. Lombardo, and Wilson KS Chiu, "Quantitative
-    x-ray phase imaging at the nanoscale by multilayer Laue lenses," Scientific 
+    x-ray phase imaging at the nanoscale by multilayer Laue lenses," Scientific
     reports 3 (2013).
-        
+
     """
-    
+
     rows, cols = gx.shape
-    
+
     pad = 2 * padding + 1
-    
+
     gx_padding = np.zeros((pad * rows, pad * cols), dtype='d')
     gy_padding = np.zeros((pad * rows, pad * cols), dtype='d')
-    
+
     gx_padding[(pad // 2) * rows : (pad // 2 + 1) * rows,
                (pad // 2) * cols : (pad // 2 + 1) * cols] = gx
-    gy_padding[(pad // 2) * rows : (pad // 2 + 1) * rows, 
+    gy_padding[(pad // 2) * rows : (pad // 2 + 1) * rows,
                (pad // 2) * cols : (pad // 2 + 1) * cols] = gy
-    
+
     tx = np.fft.fftshift(np.fft.fft2(gx_padding))
     ty = np.fft.fftshift(np.fft.fft2(gy_padding))
-    
+
     c = np.zeros((pad * rows, pad * cols), dtype=complex)
-    
+
     mid_col = pad * cols // 2.0 + 1
     mid_row = pad * rows // 2.0 + 1
-    
+
     ax = 2 * np.pi * (np.arange(pad * cols) + 1 - mid_col) / (pad * cols * dx)
     ay = 2 * np.pi * (np.arange(pad * rows) + 1 - mid_row) / (pad * rows * dy)
-    
+
     kappax, kappay = np.meshgrid(ax, ay)
-    
+
     c = -1j * (kappax * tx + w * kappay * ty)
-    
+
     c = np.ma.masked_values(c, 0)
     c /= (kappax**2 + w * kappay**2)
     c = np.ma.filled(c, 0)
-    
+
     c = np.fft.ifftshift(c)
     phi_padding = np.fft.ifft2(c)
     phi_padding = -phi_padding.real
-    
+
     phi = phi_padding[(pad // 2) * rows : (pad // 2 + 1) * rows,
                       (pad // 2) * cols : (pad // 2 + 1) * cols]
-    
+
     return phi
 
 
 
-def dpc_runner(start_point, pixel_size, focus_to_det, rows, cols, dx, dy, 
-               energy, roi, bad_pixels, solver, ref, image_sequence, padding=0, 
+def dpc_runner(start_point, pixel_size, focus_to_det, rows, cols, dx, dy,
+               energy, roi, bad_pixels, solver, ref, image_sequence, padding=0,
                w=1., scale=True):
     """
     Controller function to run the whole DPC
-    
+
     Parameters
     ----------
     start_point : 2-element list
         start_point[0], start-searching point for the intensity attenuation
         start_point[1], start-searching point for the phase gradient
-    
+
     pixel_size : integer
         real physical pixel size of the detector in um
-    
+
     focus_to_det : float
         focus to detector distance in um
-    
+
     rows : integer
-        number of scanned rows 
-    
+        number of scanned rows
+
     cols : integer
         number of scanned columns
-    
+
     dx : float
         scanning step size in x direction (in micro-meter)
-        
+
     dy : float
         scanning step size in y direction (in micro-meter)
-    
+
     energy : float
         energy of the scanning x-ray in keV
-    
+
     roi : numpy.ndarray, optional
-        upper left co-ordinates of roi's and the, length and width of roi's 
+        upper left co-ordinates of roi's and the, length and width of roi's
         from those co-ordinates
-    
+
     bad_pixels : list
         store the coordinates of bad pixels
         [(1, 5), (2, 6)] --> 2 bad pixels --> (1, 5) and (2, 6)
-    
+
     solver : string
         type of solver, one of the following
         * 'Nelder-Mead'
@@ -368,82 +368,82 @@ def dpc_runner(start_point, pixel_size, focus_to_det, rows, cols, dx, dy,
         * 'SLSQP'
         * 'dogleg'
         * 'trust-ncg'
-    
+
     ref : 2-D numpy array
         the reference image for a DPC calculation
-        
+
     image_sequence : iteratable image object
         return diffraction patterns (2D Numpy arrays) when iterated over
-    
+
     padding : integer, optional
         padding parameter
         default value, padding = 0 --> no padding
                         p p p
         padding = 1 --> p v p
                         p p p
-    
+
     w : float, optional
         weighting parameter for the phase gradient along x and y direction when
         constructing the final phase image
         default value = 0, which means that gx and gy equally contribute to
         the final phase image
-    
+
     scale : bool, optional
         if True, scale gx and gy according to the experiment set up
         if False, ignore pixel_size, focus_to_det, energy
-        
+
     Returns
     -------
     phi : 2-D numpy array
         the final reconstructed phase image
-    
+
     """
-    
+
     # Initialize a, gx, gy and phi
     a = np.zeros((rows, cols), dtype='d')
     gx = np.zeros((rows, cols), dtype='d')
     gy = np.zeros((rows, cols), dtype='d')
     phi = np.zeros((rows, cols), dtype='d')
-    
+
     # Dimension reduction along x and y direction
     refx, refy = image_reduction(ref, roi, bad_pixels)
-    
+
     # 1-D IFFT
     ref_fx = np.fft.fftshift(np.fft.ifft(refx))
     ref_fy = np.fft.fftshift(np.fft.ifft(refy))
-    
+
     ffx = _rss_factory(len(ref_fx))
     ffy = _rss_factory(len(ref_fy))
-    
+
     # Same calculation on each diffraction pattern
     for index, im in enumerate(image_sequence):
         i, j = np.unravel_index(index, (rows, cols))
-        
+
         # Dimension reduction along x and y direction
         imx, imy = image_reduction(im, roi, bad_pixels)
-                
+
         # 1-D IFFT
         fx = np.fft.fftshift(np.fft.ifft(imx))
         fy = np.fft.fftshift(np.fft.ifft(imy))
-                
+
         # Nonlinear fitting
         _a, _gx = dpc_fit(ffx, ref_fx, fx, start_point, solver)
         _a, _gy = dpc_fit(ffy, ref_fy, fy, start_point, solver)
-                            
+
         # Store one-point intermediate results
         gx[i, j] = _gx
         gy[i, j] = _gy
         a[i, j] = _a
-    
+
     if scale is True:
         # lambda = h * c / E
         lambda_ = 12.4e-4 / energy
         gx *= - len(ref_fx) * pixel_size / (lambda_ * focus_to_det)
         gy *= len(ref_fy) * pixel_size / (lambda_ * focus_to_det)
-    
+
     # Reconstruct the final phase image
     phi = recon(gx, gy, dx, dy, padding, w)
-    
+
     return phi
 
 # attributes
--- skxray/tests/test_dpc.py
+++ skxray/tests/test_dpc.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # ######################################################################
 # Copyright (c) 2014, Brookhaven Science Associates, Brookhaven        #
 # National Laboratory. All rights reserved.                            #
@@ -34,135 +33,135 @@
 # POSSIBILITY OF SUCH DAMAGE.                                          #
 ########################################################################
 """
-This is a unit/integrated testing script for dpc.py, which conducts 
+This is a unit/integrated testing script for dpc.py, which conducts
 Differential Phase Contrast (DPC) imaging based on Fourier-shift fitting.
-    
+
 """
-  
+
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
-                            
+
 import numpy as np
 from numpy.testing import (assert_array_equal, assert_array_almost_equal,
                            assert_almost_equal)
 
-import dpc
+import skxray.dpc as dpc
 
 
 def test_image_reduction_default():
     """
-    Test image reduction when default parameters (roi and bad_pixels) are used 
-    
+    Test image reduction when default parameters (roi and bad_pixels) are used
+
     """
-    
+
     # Generate a 2D matrix
     img = np.arange(100).reshape(10, 10)
-    
+
     # Expected results
     xsum = [450, 460, 470, 480, 490, 500, 510, 520, 530, 540]
     ysum = [45, 145, 245, 345, 445, 545, 645, 745, 845, 945]
-    
+
     # call image reduction
     xline, yline = dpc.image_reduction(img)
-    
+
     assert_array_equal(xline, xsum)
-    
+
     assert_array_equal(yline, ysum)
-    
+
 
 def test_image_reduction():
     """
     Test image reduction when the following parameters are used:
     roi = (3, 3, 5, 5);
     bad_pixels = [(0, 1), (4, 4), (7, 8)]
-    
+
     """
 
     # generate a 2D matrix
-    img = np.arange(100).reshape(10, 10)    
-    
+    img = np.arange(100).reshape(10, 10)
+
     # set up roi and bad_pixels
     roi = (3, 3, 5, 5)
     bad_pixels = [(0, 1), (4, 4), (7, 8)]
-    
+
     # Expected results
     xsum = [265, 226, 275, 280, 285]
     ysum = [175, 181, 275, 325, 375]
-    
+
     # call image reduction
-    xline, yline = dpc.image_reduction(img, roi, bad_pixels)    
-    
+    xline, yline = dpc.image_reduction(img, roi, bad_pixels)
+
     assert_array_equal(xline, xsum)
-    
+
     assert_array_equal(yline, ysum)
-    
-    
+
+
 def test_rss():
     """
     Test _rss
-    
+
     """
-    
+
     xdata = np.arange(10)
     ydata = [0, 1.68770792 + 1.07314584j, -3.64452105 - 1.64847394j,
-             5.76102172 + 1.67649299j, -7.91993997 - 1.12896006j, 
-             10.00000000 + 0.j, -11.87990996 + 1.6934401j, 
-             13.44238401 - 3.91181697j, -14.57808419 + 6.59389576j, 
+             5.76102172 + 1.67649299j, -7.91993997 - 1.12896006j,
+             10.00000000 + 0.j, -11.87990996 + 1.6934401j,
+             13.44238401 - 3.91181697j, -14.57808419 + 6.59389576j,
              15.18937126 - 9.65831252j]
     rss = dpc._rss_factory(len(xdata))
     v = [2, 3]
-    
+
     residue = rss(v, xdata, ydata)
-    
+
     assert_almost_equal(residue, 0)
-    
+
 
 def test_dpc_fit():
     """
     Test dpc_fit
-    
+
     """
-    
+
     start_point = [1, 0]
     solver = 'Nelder-Mead'
-    
+
     # Test 1 (succeeded): res = [1.34, 0.23]
     xdata = np.arange(10)
     ydata = [0, 0.81179901 - 1.06610617j, 2.06693932 - 1.70591965j,
-             3.60213104 - 1.78467139j, 5.21885188 - 1.22195953j, 
-             6.70000000 + 0.j, 7.82827782 + 1.83293929j, 
-             8.40497243 + 4.16423324j, 8.26775728 + 6.82367859j, 
+             3.60213104 - 1.78467139j, 5.21885188 - 1.22195953j,
+             6.70000000 + 0.j, 7.82827782 + 1.83293929j,
+             8.40497243 + 4.16423324j, 8.26775728 + 6.82367859j,
              7.30619109 + 9.59495554j]
     rss = dpc._rss_factory(len(ydata))
     res = dpc.dpc_fit(rss, xdata, ydata, start_point, solver)
     assert_array_almost_equal(res, [1.34, 0.23])
-    
+
     # Test 2 (succeeded): res = [0.88, 0.28]
     ydata = [0, 0.38340055 - 0.79208839j, 1.17473457 - 1.31057189j,
-             2.23675349 - 1.40233156j,  3.38291514 - 0.97277188j, 
-             4.40000000 + 0.j, 5.07437271 + 1.45915782j, 
+             2.23675349 - 1.40233156j,  3.38291514 - 0.97277188j,
+             4.40000000 + 0.j, 5.07437271 + 1.45915782j,
              5.21909148 + 3.27210698j, 4.69893829 + 5.24228756j,
              3.45060497 + 7.1287955j]
     res = dpc.dpc_fit(rss, xdata, ydata, start_point, solver)
-    assert_array_almost_equal(res, [0.88, 0.28]) 
-    
+    assert_array_almost_equal(res, [0.88, 0.28])
+
     """
     # Test 3 (failed): res = [-0.25595591, -0.27603199]
     ydata = [0, -0.71170192 - 0.31918705j, -0.70539481 - 1.3914087j,
              0.48961848 - 2.28820317j, 2.42602688 - 1.96183423j, 3.9,
-             3.63904032 + 2.94275135j, 1.14244312 + 5.33914073j, 
+             3.63904032 + 2.94275135j, 1.14244312 + 5.33914073j,
              -2.82157925 + 5.56563478j, -6.40531730+2.87268347j]
     res = dpc.dpc_fit(xdata, ydata, start_point, solver)
-    assert_array_almost_equal(res, [0.78, 0.68])    
-    """    
-   
+    assert_array_almost_equal(res, [0.78, 0.68])
+    """
+
 
 def test_dpc_end_to_end():
     """
     Integrated test for DPC based on dpc_runner
-    
+
     """
-    
+
     start_point = [1, 0]
     pixel_size = 55
     focus_to_det = 1.46e6
@@ -178,23 +177,22 @@ def test_dpc_end_to_end():
     solver = 'Nelder-Mead'
     img_size = (40, 40)
     scale = True
-    
+
     ref_image = np.ones(img_size)
     image_sequence = np.ones((rows * cols, img_size[0], img_size[1]))
 
-    phi = dpc.dpc_runner(start_point, pixel_size, focus_to_det, rows, cols, dx, 
-                         dy, energy, roi, bad_pixels, solver, ref_image, 
+    phi = dpc.dpc_runner(start_point, pixel_size, focus_to_det, rows, cols, dx,
+                         dy, energy, roi, bad_pixels, solver, ref_image,
                          image_sequence, padding, w, scale)
-    
+
     assert_array_almost_equal(phi, np.zeros((rows, cols)))
 
-   
+
 if __name__ == "__main__":
 
     test_image_reduction_default()
     test_image_reduction()
     test_rss()
     test_dpc_fit()
-    
-    test_dpc_end_to_end()
 
+    test_dpc_end_to_end()
