2304d6358b78004184b4f57d6b4161c74379c7dc
Gabriel Iltis <giltis@bnl.gov>
2014-12-08 10:21:23 -0500

    DEV: Started conversion of img_proc funcs to api-based config
    Added new file named ff_image.py that creates new namespace for
    full-field imaging, and will initially import, and enable access to
    all image processing tools and functions that are to be used to process
    3D image data sets (e.g. 3D tomography)
    
    Current addition: Filtering operations imported from skimage

---
+++ skxray/img_proc/filterops.py
@@ -0,0 +1,23 @@
+# Module for the BNL image processing project
+# Developed at the NSLS-II, Brookhaven National Laboratory
+# Developed by Gabriel Iltis, Oct. 2013
+"""
+This module contains functions used for filtering images and volume data sets.
+The basic filters included in this module utilize the filter set included in
+scipy.ndimage.filters. More advanced filtering operations will be added 
+shortly.
+"""
+
+
+#Need additional work in order to implement in vistrails since the function
+#to apply needs to be defined in order to use these filters.
+# from scipy.ndimage.filters import generic_filter
+# from scipy.ndimage.filters import generic_gradient_magnitude
+# from scipy.ndimage.filters import generic_laplace
+# from scipy.ndimage.filters import uniform_filter
+# from scipy.ndimage.fourier import fourier_ellipsoid
+# from scipy.ndimage.fourier import fourier_gaussian
+# from scipy.ndimage.fourier import fourier_shift
+# from scipy.ndimage.fourier import fourier_uniform
+
+
