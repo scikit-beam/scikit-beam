74e80e642e5f0016c92ef691a75d4fc706655c8c
danielballan <daniel.b.allan@gmail.com>
2015-04-27 11:03:15 -0400

    DOC: Document API change in core funcs.

---
+++ doc/api_changes/2015-04-27-core.py
@@ -0,0 +1,7 @@
+Grid-building functions
+-----------------------
+
+:func:`pixels_to_radius` and :func:`pixels_to_phi` were renamed to
+:func:`radial_grid` and :func:`angle_grid` respectively. The name and order
+of their arguments was also changed: see their docstring or API docs for
+details.
