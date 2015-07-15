0bfddbabaf2a80c8ec69e3105c2fe54a40b7c902
Eric Dill <edill@bnl.gov>
2015-07-15 16:08:37 -0400

    MNT: Remove examples from scikit-xray
    All examples live in scikit-xray-examples

--- skxray/examples/tests/test_xrf_spectrum_demo.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import, division, print_function
-
-def test_xrf_spectrum_demo():
-    # smoketest the demo
-    from ..demo_xrf_spectrum import run_demo
-    import matplotlib.pyplot as plt
-    plt.ion()
-    run_demo()
