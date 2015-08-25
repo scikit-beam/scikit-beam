dc71c13b71c9803e4b64e146d4ca117fb34ac961
Eric Dill <edill@bnl.gov>
2015-08-25 12:06:12 -0400

    Add template for py2/py3 c extension

--- src/ctrans.c
+++ src/ctrans.c
@@ -13,6 +13,8 @@
 #include <pthread.h>
 #endif
 
+/* Set up */
+
 #define HC_OVER_E 12398.4
 
 #define true -1
@@ -74,6 +76,8 @@ static PyMethodDef _ctransMethods[] = {
   {NULL, NULL, 0, NULL}     /* Sentinel - marks the end of this structure */
 };
 
+
+/* Computation functions */
 static PyObject* ccdToQ(PyObject *self, PyObject *args, PyObject *kwargs){
   static char *kwlist[] = { "angles", "mode", "ccd_size", "ccd_pixsize",
 			    "ccd_cen", "dist", "wavelength",
@@ -542,4 +546,84 @@ unsigned long c_grid3d(double *dout, unsigned long *nout, double *standarderror,
 PyMODINIT_FUNC initctrans(void)  {
 	(void) Py_InitModule3("ctrans", _ctransMethods, _ctransDoc);
 	import_array();  // Must be present for NumPy.  Called first after above line.
+}
+struct module_state {
+    PyObject *error;
+};
+
+#if PY_MAJOR_VERSION >= 3
+#define GETSTATE(m) ((struct module_state*)PyModule_GetState(m))
+#else
+#define GETSTATE(m) (&_state)
+static struct module_state _state;
+#endif
+
+static PyObject *
+error_out(PyObject *m) {
+    struct module_state *st = GETSTATE(m);
+    PyErr_SetString(st->error, "something bad happened");
+    return NULL;
+}
+
+static PyMethodDef myextension_methods[] = {
+    {"error_out", (PyCFunction)error_out, METH_NOARGS, NULL},
+    {NULL, NULL}
+};
+
+#if PY_MAJOR_VERSION >= 3
+
+static int myextension_traverse(PyObject *m, visitproc visit, void *arg) {
+    Py_VISIT(GETSTATE(m)->error);
+    return 0;
+}
+
+static int myextension_clear(PyObject *m) {
+    Py_CLEAR(GETSTATE(m)->error);
+    return 0;
+}
+
+
+static struct PyModuleDef moduledef = {
+        PyModuleDef_HEAD_INIT,
+        "myextension",
+        NULL,
+        sizeof(struct module_state),
+        myextension_methods,
+        NULL,
+        myextension_traverse,
+        myextension_clear,
+        NULL
+};
+
+#define INITERROR return NULL
+
+PyObject *
+PyInit_myextension(void)
+
+#else
+#define INITERROR return
+
+void
+initmyextension(void)
+#endif
+{
+#if PY_MAJOR_VERSION >= 3
+    PyObject *module = PyModule_Create(&moduledef);
+#else
+    PyObject *module = Py_InitModule("myextension", myextension_methods);
+#endif
+
+    if (module == NULL)
+        INITERROR;
+    struct module_state *st = GETSTATE(module);
+
+    st->error = PyErr_NewException("myextension.Error", NULL, NULL);
+    if (st->error == NULL) {
+        Py_DECREF(module);
+        INITERROR;
+    }
+
+#if PY_MAJOR_VERSION >= 3
+    return module;
+#endif
 }
