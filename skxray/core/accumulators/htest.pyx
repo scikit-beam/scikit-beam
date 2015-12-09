945309a3897b14668dc01b361ae446ed8c3eab2c
Pavol Juhas <pavol.juhas@gmail.com>
2015-12-09 15:41:53 -0500

    Remove class hist2d - leave it for later.
    Focus on hist1d.  Also fix comma spacing.

--- skxray/core/accumulators/htest.pyx
+++ skxray/core/accumulators/htest.pyx
@@ -17,7 +17,7 @@ ctypedef fused hnumtype:
 
 # Histogramming classes
 class histaxis:
-    def __init__(self,nbin,low,high):
+    def __init__(self, nbin, low, high):
         self.low = low
         self.high = high
         self.nbin = nbin
@@ -30,32 +30,32 @@ class histaxis:
         return iidx
 
     def values(self):
-        return np.linspace(self.low+0.5*self.binsize,self.high-0.5*self.binsize,self.nbin)
+        return np.linspace(self.low+0.5*self.binsize, self.high-0.5*self.binsize, self.nbin)
 
 class hist1d:
-    def __init__(self,nbinx,xlow,xhigh):
+    def __init__(self, nbinx, xlow, xhigh):
         self.data = np.zeros(nbinx)
         #cdef float self.data[nbinx]
         self.nbinx = nbinx
-        self.xaxis = histaxis(nbinx,xlow,xhigh)
+        self.xaxis = histaxis(nbinx, xlow, xhigh)
 
-    def fillnp(self,xval,weight):
+    def fillnp(self, xval, weight):
         xbin=self.xaxis.bin(xval)
         inside = (0 <= xbin) & (xbin < self.nbinx)
         xbinin = xbin[inside]
         self.data += np.bincount(xbinin, weight[inside], self.nbinx)
 
-    def fill(self,xval,weight):
+    def fill(self, xval, weight):
         low = self.xaxis.low
         binsize = self.xaxis.binsize
 
-        for val, wt in zip(xval,weight):
+        for val, wt in zip(xval, weight):
             fidx = (val - low) / binsize
             iidx = np.floor(fidx).astype(int)
             if iidx >= 0 and iidx < self.nbinx:
                 self.data[iidx] += wt
 
-    def fillcywithcall(self,np.ndarray[hnumtype, ndim=1] xval, np.ndarray[hnumtype, ndim=1] weight):
+    def fillcywithcall(self, np.ndarray[hnumtype, ndim=1] xval, np.ndarray[hnumtype, ndim=1] weight):
         cdef np.ndarray[np.float_t, ndim=1] data = self.data
         cdef float low = self.xaxis.low
         cdef float high = self.xaxis.high
@@ -73,7 +73,7 @@ class hist1d:
 
     @cython.boundscheck(False)
     @cython.wraparound(False)
-    def fillcy(self,np.ndarray[hnumtype, ndim=1] xval, np.ndarray[hnumtype, ndim=1] weight):
+    def fillcy(self, np.ndarray[hnumtype, ndim=1] xval, np.ndarray[hnumtype, ndim=1] weight):
 
         cdef float low = self.xaxis.low
         cdef float high = self.xaxis.high
@@ -108,16 +108,3 @@ cdef void fillonecy(hnumtype xval, hnumtype weight,
     iidx = int((xval - low) / binsize)
     pdata[iidx] += weight
     return
-
-
-class hist2d:
-    def __init__(self,nbinx,xlow,xhigh,nbiny,ylow,yhigh):
-        self.data = np.zeros((nbinx,nbiny))
-        self.xaxis = histaxis(nbinx,xlow,xhigh)
-        self.yaxis = histaxis(nbiny,ylow,yhigh)
-    def fill(self,xval,yval,weight=1.0):
-        xbin=self.xaxis.bin(xval)
-        ybin=self.yaxis.bin(yval)
-        if xbin>=0 and xbin<self.xaxis.nbin and ybin>=0 and ybin<self.yaxis.nbin:
-            self.data[xbin,ybin] += weight
-        return
