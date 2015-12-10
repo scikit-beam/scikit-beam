6a5e53187429a30482f640cfe0c617d3d916d1d7
Eric Dill <edill@bnl.gov>
2015-12-10 14:30:08 -0500

    TST: generator based histogram testing

--- skxray/core/accumulators/tests/test_histogram.py
+++ skxray/core/accumulators/tests/test_histogram.py
@@ -3,33 +3,30 @@ from numpy.testing import assert_array_equal
 from numpy.testing import assert_array_almost_equal
 from skxray.core.accumulators.histogram import Histogram
 
-x = np.random.random(1000000)*40
-xi = x.astype(int)
-w = np.linspace(1, 10, len(x))
-wi = w.astype(int)
 
-def test_histfloat():
-    h = Histogram([10, 0, 10.01])
-    h.fill(x, weights=w)
+def _1d_histogram_tester(binlowhighs, *coords, weights):
+    h = Histogram(binlowhighs)
+    h.fill(coords, weights=weights)
     ynp = np.histogram(x, h.edges[0], weights=w)[0]
     assert_array_almost_equal(ynp, h.values)
-    h.reset()
-    h.fill(x)
-    ynp = np.histogram(x, h.edges[0])[0]
-    assert_array_almost_equal(ynp, h.values)
-    return
 
 
-def test_histint():
-    h = Histogram([10, 0, 10.01])
-    h.fill(xi, weights=wi)
-    ynp = np.histogram(xi, h.edges[0], weights=wi)[0]
-    assert_array_equal(ynp, h.values)
-    h.reset()
-    h.fill(xi)
-    ynp = np.histogram(xi, h.edges[0])[0]
-    assert_array_equal(ynp, h.values)
-    return
+def test_1d_histogram():
+    binlowhigh = [10, 0, 10.01]
+    x = np.random.random(1000000)*40
+    xi = x.astype(int)
+    w = np.linspace(1, 10, len(x))
+    wi = w.copy()
+    vals = [
+        [binlowhigh, x, w],
+        [binlowhigh, x, 1],
+        [binlowhigh, xi, wi],
+        [binlowhigh, xi, 1],
+        [binlowhigh, x, wi],
+        [binlowhigh, xi, w],
+    ]
+    for binlowhigh, x, w in vals:
+        yield _1d_histogram_tester, binlowhigh, x, w
 
 
 def test_argtypes():
@@ -45,5 +42,4 @@ def test_argtypes():
 
 
 if __name__ == '__main__':
-    test_histfloat()
-    test_histint()
+    test_1d_histogram()
