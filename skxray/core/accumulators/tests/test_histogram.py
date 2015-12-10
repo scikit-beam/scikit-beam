88fa7890fa125a0c99872f4e759900cb9255de74
Eric Dill <edill@bnl.gov>
2015-12-10 15:06:27 -0500

    TST: Finish generator testing of 1D histogram

--- skxray/core/accumulators/tests/test_histogram.py
+++ skxray/core/accumulators/tests/test_histogram.py
@@ -7,38 +7,30 @@ from skxray.core.accumulators.histogram import Histogram
 def _1d_histogram_tester(binlowhighs, coords, weights=1):
     h = Histogram(binlowhighs)
     h.fill(coords, weights=weights)
-    ynp = np.histogram(coords[0], h.edges[0], weights=w)[0]
+    if np.isscalar(weights):
+        ynp = np.histogram(coords, h.edges[0])[0]
+    else:
+        ynp = np.histogram(coords, h.edges[0], weights=weights)[0]
+
     assert_array_almost_equal(ynp, h.values)
 
 
 def test_1d_histogram():
     binlowhigh = [10, 0, 10.01]
-    x = np.random.random(1000000)*40
-    xi = x.astype(int)
-    w = np.linspace(1, 10, len(x))
-    wi = w.copy()
+    xf = np.random.random(1000000)*40
+    xi = xf.astype(int)
+    wf = np.linspace(1, 10, len(xf))
+    wi = wf.copy()
     vals = [
-        [binlowhigh, x, w],
-        [binlowhigh, x, 1],
+        [binlowhigh, xf, wf],
+        [binlowhigh, xf, 1],
         [binlowhigh, xi, wi],
         [binlowhigh, xi, 1],
-        [binlowhigh, x, wi],
-        [binlowhigh, xi, w],
+        [binlowhigh, xf, wi],
+        [binlowhigh, xi, wf],
     ]
     for binlowhigh, x, w in vals:
         yield _1d_histogram_tester, binlowhigh, x, w
-
-
-def test_argtypes():
-    h = Histogram([10, 0, 10.01])
-    h.fill(x, weights=w)
-    h.fill(x, weights=wi)
-    h.fill(xi, weights=w)
-    h.fill(xi, weights=wi)
-    h.fill(x)
-    h.fill(xi)
-    h.fill(x, weights=1.0)
-    return
 
 
 if __name__ == '__main__':
