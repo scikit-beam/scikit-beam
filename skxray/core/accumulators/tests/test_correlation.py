57c99f8d9cfd4f395af7d9f1bd7e923c851e32d9
Eric Dill <edill@bnl.gov>
2015-12-31 11:23:45 -0500

    MNT: Clean up accumulator correlation test

--- skxray/core/accumulators/tests/test_correlation.py
+++ skxray/core/accumulators/tests/test_correlation.py
@@ -1,25 +1,22 @@
 from skxray.core.correlation.correlation import multi_tau_auto_corr
 from skxray.core.accumulators.correlation import (MultiTauCorrelation,
                                                   intermediate_data)
+from skxray.core.accumulators.corr_gen import lazy_correlation
 import numpy as np
-import pandas as pd
+
 # turn off auto wrapping of pandas dataframes
 pd.set_option('display.expand_frame_repr', False)
 
+num_levels = None
+num_bufs = None
+xdim = None
+ydim = None
+stack_size = None
+img_stack = None
+rois = None
+
 
 def test_against_reference_implementation():
-    num_levels = 6
-    num_bufs = 4  # must be even
-    xdim = 256
-    ydim = 512
-    stack_size = 100
-    # img_stack = np.zeros((stack_size, xdim, ydim), dtype=int)
-    img_stack = np.random.randint(1, 10, ((stack_size, xdim, ydim)))
-    rois = np.zeros_like(img_stack[0])
-    # make sure that the ROIs can be any integers greater than 1. They do not
-    # have to start at 1 and be continuous
-    rois[0:xdim//10, 0:ydim//10] = 5
-    rois[xdim//10:xdim//5, ydim//10:ydim//5] = 3
     # run the correlation with the reference implementation
     g2, lag_steps = multi_tau_auto_corr(num_levels, num_bufs, rois, img_stack)
     # run the correlation with the accumulator version
@@ -39,3 +36,30 @@ def test_against_reference_implementation():
     # compare the results
     assert np.all(mt.g2 == g2)
     assert np.all(mt.lag_steps == lag_steps)
+
+
+def setup():
+    global num_levels, num_bufs, xdim, ydim, stack_size, img_stack, rois
+    num_levels = 6
+    num_bufs = 4  # must be even
+    xdim = 256
+    ydim = 512
+    stack_size = 100
+    img_stack = np.random.randint(1, 10, (stack_size, xdim, ydim))
+    rois = np.zeros_like(img_stack[0])
+    # make sure that the ROIs can be any integers greater than 1. They do not
+    # have to start at 1 and be continuous
+    rois[0:xdim//10, 0:ydim//10] = 5
+    rois[xdim//10:xdim//5, ydim//10:ydim//5] = 3
+
+
+def test_generator_against_reference():
+    # run the correlation with the reference implementation
+    g2, lag_steps = multi_tau_auto_corr(num_levels, num_bufs, rois, img_stack)
+    gen = lazy_correlation(num_levels, num_bufs, img_stack, rois)
+    all_res = list(gen)
+    final_res = all_res[-1]
+    assert np.all(final_res.g2 == g2)
+    assert np.all(final_res.lag_steps == lag_steps)
+
+
