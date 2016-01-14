453300c52e1678ca20e1faa2a227ea9fc854559c
Eric Dill <edill@bnl.gov>
2016-01-14 16:28:05 -0500

    ENH: Auto-transform the example notebooks to rst

--- doc/tutorial.rst
+++ doc/tutorial.rst
@@ -9,10 +9,18 @@ Basic Usage
 .. toctree::
    :maxdepth: 1
 
-  tutorial/dpc_demo
-  tutorial/speckle-plotting
-  tutorial/XPCS_fitting_with_lmfit
-  tutorial/Multi_tau_one_time_correlation_example
-  tutorial/Generate_ROI_labeled_arrays
-  tutorial/recip_example
-  tutorial/plot_xrf_spectrum
+   tutorial/X-ray_Speckle_Visibility_Spectroscopy
+   tutorial/Histogram2D-RingAveraging
+   tutorial/Multi_tau_one_time_correlation_example
+   tutorial/partial-multi-tau
+   tutorial/D_estimate_demo
+   tutorial/IXS-alignment-clean
+   tutorial/Histogram2D
+   tutorial/Generate_ROI_labeled_arrays
+   tutorial/two-time-correlation
+   tutorial/recip_example
+   tutorial/XPCS_fitting_with_lmfit
+   tutorial/plot_xrf_spectrum
+   tutorial/dpc_demo
+   tutorial/speckle-plotting
+   tutorial/histogram3D
--- doc/tutorial/write_main_index.py
+++ doc/tutorial/write_main_index.py
@@ -1,25 +1,38 @@
 import os
 import tempfile
+import glob
+import shutil
 # make the index page
 # I originally wanted to use jinja2 to do this, but the CSS has double curly
 # brackets, which jinja does not like, so here we go!
+examples_dir = os.path.join(tempfile.gettempdir(), 'scikit-beam-examples')
 output_dir = os.path.join(tempfile.gettempdir(), 'notebooks')
-notebooks = sorted([f for f in os.listdir(output_dir) if f.endswith('.ipynb')])
+if os.path.exists(output_dir):
+    shutil.rmtree(output_dir)
+    os.mkdir(output_dir)
+# copy all notebooks from the git repo to /tmp/notebooks
+nbs = glob.glob('/tmp/scikit-beam-examples/**/*.ipynb', recursive=True)
 
+for nb in nbs:
+    shutil.copy(nb, os.path.join(output_dir, os.path.split(nb)[1]))
+notebooks = os.listdir(output_dir)
 # read in the template
-with open('../index.rst.tmpl', 'r') as f:
-    new_file = []
-    for line in f.readlines():
-        if line.endswith('\n'):
-            line = line[:-1]
-        # find the line where I want to write the list of examples
-        if 'REPLACE_WITH_LIST_OF_EXAMPLES' in line:
-            line = ['   tutorial/%s' % os.path.splitext(notebook_path)[0]
-                    for notebook_path in notebooks]
-            new_file.extend(line)
-        else:
-            new_file.append(line)
+files_to_convert = {'../index.rst.tmpl': '../index.rst',
+                    '../tutorial.rst.tmpl': '../tutorial.rst'}
+for template_file, output_file in files_to_convert.items():
+    with open(template_file, 'r') as f:
+        new_file = []
+        for line in f.readlines():
+            if line.endswith('\n'):
+                line = line[:-1]
+            # find the line where I want to write the list of examples
+            if 'REPLACE_WITH_LIST_OF_EXAMPLES' in line:
+                line = ['   tutorial/%s' % os.path.splitext(notebook_path)[0]
+                        for notebook_path in notebooks]
+                new_file.extend(line)
+            else:
+                new_file.append(line)
 
-with open('../index.rst', 'w') as f:
-    output = '\n'.join(new_file)
-    f.write(output)
+    with open(output_file, 'w') as f:
+        output = '\n'.join(new_file)
+        f.write(output)
