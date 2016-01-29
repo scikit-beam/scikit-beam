9186ddbe559ef79e98c45fb97ba271270c512d3e
Sameera Abeykoon <sameera@bnl.gov>
2016-01-28 22:47:06 -0500

    BUG: fixed the typo of pat_img

--- skbeam/core/correlation.py
+++ skbeam/core/correlation.py
@@ -117,7 +117,7 @@ def _one_time_process(buf, G, past_intensity_norm, future_intensity_norm,
         ind = int(t_index - lev_len[:level].sum())
         normalize = img_per_level[level] - i - norm[level+1][ind]
 
-        # take out the pat_ing and future_img created using bad images
+        # take out the past_ing and future_img created using bad images
         # (bad images are converted to np.nan array)
         if np.isnan(past_img).any() or np.isnan(future_img).any():
             norm[level + 1][ind] += 1
