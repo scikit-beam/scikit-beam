a8b2c137525cd86d37daa1cc9a155a24de093b5a
Eric Dill <edill@bnl.gov>
2016-02-03 14:41:31 -0500

    DOC: Pull examples from scikit-beam:master

--- doc/example/get_examples_repo.py
+++ doc/example/get_examples_repo.py
@@ -4,11 +4,11 @@ import os
 clone_dir = os.path.join(tempfile.gettempdir(), 'scikit-beam-examples')
 try:
     ret = subprocess.check_output(
-        ['git', 'clone', 'https://github.com/ericdill/scikit-beam-examples',
+        ['git', 'clone', 'https://github.com/scikit-beam/scikit-beam-examples',
          clone_dir])
 except subprocess.CalledProcessError:
     print("scikit-beam-examples already exists at %s" % (clone_dir))
     print("resetting to the master branch")
     subprocess.Popen(['git', 'remote', 'update'], cwd=clone_dir)
-    subprocess.Popen(['git', 'reset', '--hard', 'origin/update-notebook-headers'],
+    subprocess.Popen(['git', 'reset', '--hard', 'origin/master'],
                      cwd=clone_dir)
