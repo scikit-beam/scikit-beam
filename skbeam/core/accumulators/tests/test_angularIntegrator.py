98a9d73540c20776e77d5eb6c63e51e73542d8c6
Christopher O'Grady <cpo@slac.stanford.edu>
2016-03-13 19:45:05 -0700

    tweak interface

--- skbeam/core/accumulators/tests/test_angularIntegrator.py
+++ skbeam/core/accumulators/tests/test_angularIntegrator.py
@@ -1,31 +1,5 @@
-from skbeam.core.accumulators.AngularIntegrator import AngularIntegrator
+from skbeam.core.accumulators.angularIntegrator import AngularIntegrator
 import numpy as np
-from time import time
-import matplotlib.pyplot as plt
-
-
-import matplotlib
-import matplotlib.pyplot as plt
-
-def drawImage(arr, img_range=None, amp_range=None, figsize=(10,10)) :    # range = (left, right, low, high), amp_range=(zmin,zmax)
-    fig = plt.figure(figsize=figsize, dpi=80, facecolor='w', edgecolor='w', frameon=True)
-    fig.subplots_adjust(left=0.05, bottom=0.03, right=0.98, top=0.98, wspace=0.2, hspace=0.1)
-    figAxes = fig.add_subplot(111)
-    #figAxes = fig.add_axes([0.15, 0.06, 0.78, 0.21])
-    imAxes = figAxes.imshow(arr, origin='upper', interpolation='nearest', aspect='auto', extent=img_range)
-    if amp_range != None : imAxes.set_clim(amp_range[0],amp_range[1])
-    colbar = fig.colorbar(imAxes, pad=0.03, fraction=0.04, shrink=1.0, aspect=40, orientation='horizontal')
-
-#------------------------------
-
-def drawGraph(x,y) : 
-    fig = plt.figure(figsize=(6,4), dpi=80, facecolor='w', edgecolor='w', frameon=True)
-    #fig.subplots_adjust(left=0.05, bottom=0.03, right=0.98, top=0.98, wspace=0.2, hspace=0.1)
-    #figAxes = fig.add_subplot(111)
-    ax = fig.add_axes([0.15, 0.10, 0.78, 0.86])
-    ax.plot(x,y,'b-')
-
-#------------------------------
 
 def MakeImage(shape=(1024,1024)) :
     # Create test image - a sinc function, centered in the middle of
@@ -49,30 +23,19 @@ def MakeImage(shape=(1024,1024)) :
     image = np.abs(np.sinc(rgrid))    
     return image
 
+import matplotlib.pyplot as plt
+
 def testAngularIntegrator():
 
-    image = np.ones([1000,1000])
-    mask  = np.ones_like(image, dtype=np.int)
+    image = MakeImage()
+    mask = np.ones_like(image, dtype=np.int)
+    normImage = np.ones_like(image, dtype=np.int)
 
-    print 'image.shape =', image.shape
-    
     ysize, xsize = image.shape
     
-    t0_sec = time()
-    angint = AngularIntegrator()
-    angint.setParameters(xsize, ysize, xc=1005, yc=690, rmin=0, rmax=1000, nbins=1000, mask=mask)
-    #angint.setParameters(xsize, ysize, rmin=1, rmax=1200, nbins=200, mask=mask)
-    print "Time consumed for indexing = %.3f sec" % (time()-t0_sec)
+    angint = AngularIntegrator(xsize, ysize, xc=410, yc=718, rmin=0, rmax=1000, nbins=1000)
 
-    plt.imshow(angint.getRBinIndexMap())
-    plt.show()
-
-    plt.imshow(image)
-    plt.show()
-
-    t0_sec = time()
-    bincent, integral = angint.getRadialHistogramArrays(image)
-    print "Time consumed for intensity binning = %.3f sec" % (time()-t0_sec)
+    bincent, integral = angint.getRadialHistogramArrays(image,norm=True)
 
     plt.plot(bincent, integral)
     plt.show()    
