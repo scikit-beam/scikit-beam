33cba26ba1087b75ffbd2dee05281223a0c04c98
Christopher O'Grady <cpo@slac.stanford.edu>
2016-04-23 11:01:06 -0700

    More style fixes

--- skbeam/core/accumulators/tests/test_projector.py
+++ skbeam/core/accumulators/tests/test_projector.py
@@ -21,8 +21,8 @@ class TestRadialProjector(object):
 
     def testRadialProjector(self):
 
-        params = [[self.image.shape[0],self.image.shape[1],False],
-                  [self.image.shape[1],self.image.shape[0],True]]
+        params = [[self.image.shape[0], self.image.shape[1],False],
+                  [self.image.shape[1], self.image.shape[0],True]]
         for xsize, ysize, cartesian in params:
             radproj = RadialProjector(xsize, ysize, xc=0, yc=0, rmin=100,
                                       rmax=900, nbins=100, phimin=5, phimax=60,
