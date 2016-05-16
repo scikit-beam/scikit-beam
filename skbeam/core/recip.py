5fe5e4d9f12c2ac36029b718bed2e46f62a26f16
CJ-Wright <cjwright4242@gmail.com>
2016-05-16 09:13:53 -0400

    ENH/DOC: change signature to r only also change the documentation

--- skbeam/core/recip.py
+++ skbeam/core/recip.py
@@ -344,21 +344,23 @@ def gisaxs(incident_beam, reflected_beam, pixel_size, detector_size,
     return gisaxs_output(alpha_i, theta_f, alpha_f, tilt_angle, qx, qy, qz, qr)
 
 
-def generate_q_bins(rmax, qmax, pixel_size, distance, wavelength):
+def generate_q_bins(rmax, pixel_size, distance, wavelength, rmin=0):
     """
     Generate the Q bins at the resolution of the detector
     Parameters
     -----------
     rmax: float
-        The maximum radial distance on the detector in distance units
-    qmax: float
-        The maximum Q on the detector
+        The maximum radial distance on the detector in distance units.
+        Note that this should go to the bottom edge of the pixel.
     pixel_size: float
         The size of the pixels, in the same units as rmax
     distance: float
         The sample to detector distance, in the same units as rmax
     wavelength: float
         The wavelength of the x-rays
+    rmin: float, optional
+        The minimum radial distance on the detector in distance units. Defaults
+        to zero. Note that this should be the bottom of the pixel
 
     Returns
     -------
@@ -366,12 +368,16 @@ def generate_q_bins(rmax, qmax, pixel_size, distance, wavelength):
         The bin edges, suitable for np.histogram or
         scipy.stats.binned_statistic
     """
-    pixel_bottom = np.arange(0, rmax, pixel_size)
+    pixel_bottom = np.arange(rmin, rmax, pixel_size)
     pixel_top = pixel_bottom + pixel_size
-    bottom_tth = np.arctan(pixel_bottom / distance)
+
+    bottom_tth = np.arctan(pixel_bottom[0] / distance)
     top_tth = np.arctan(pixel_top / distance)
-    delta_q = twotheta_to_q(top_tth, wavelength) - twotheta_to_q(bottom_tth, wavelength)
-    bottom_q = np.linspace(0, qmax, len(delta_q))
-    bins = np.zeros(len(bottom_q) + 1)
-    bins[1:] = delta_q + bottom_q
+
+    top_q = twotheta_to_q(top_tth, wavelength)
+
+    bins = np.zeros(len(top_q) + 1)
+
+    bins[0] = twotheta_to_q(bottom_tth, wavelength)
+    bins[1:] = top_q
     return bins
