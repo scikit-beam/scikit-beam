bd22b322916c61443686869009e4413131bc0161
CJ-Wright <cjwright4242@gmail.com>
2016-05-17 15:40:35 -0400

    TST: write test for ring blur mask

--- skbeam/core/tests/test_mask.py
+++ skbeam/core/tests/test_mask.py
@@ -84,3 +84,47 @@ def test_bad_to_nan_gen():
     assert np.isnan(np.asarray(y)[1]).all()
     assert np.isnan(np.asarray(y)[3]).all()
     assert not np.isnan(np.asarray(y)[4]).all()
+
+
+def test_ring_blur_mask():
+    from skbeam.core import recip
+    g = recip.geo.Geometry(
+        detector='Perkin', pixel1=.0002, pixel2=.0002,
+        dist=.23,
+        poni1=.209, poni2=.207,
+        # rot1=.0128, rot2=-.015, rot3=-5.2e-8,
+        wavelength=1.43e-11
+    )
+    r = g.rArray((2048, 2048))
+    q = g.qArray((2048, 2048)) / 10
+    # make some sample data
+    Z = 100 * np.cos(50 * r) ** 2 + 150
+
+    np.random.seed(10)
+    pixels = []
+    for i in range(0, 100):
+        a, b = np.random.randint(low=0, high=2048), np.random.randint(low=0,
+                                                                      high=2048)
+        if np.random.random() > .5:
+            # Add some hot pixels
+            Z[a, b] = np.random.randint(low=200, high=255)
+        else:
+            # and dead pixels
+            Z[a, b] = np.random.randint(low=0, high=10)
+        pixels.append((a, b))
+    bins = recip.generate_q_bins(np.max(r) - .5 * g.pixel1, g.pixel1, g.dist,
+                                 g.wavelength * 10**10)
+    msk = mask.ring_blur_mask(Z, q, (3., 3),  bins)
+    a = set(zip(*np.nonzero(~msk)))
+    b = set(pixels)
+    a_not_in_b = a - b
+    b_not_in_a = b - a
+
+    # Make certain that we have not over masked 10% of the number of bad pixels
+    assert len(a_not_in_b) / len(b) < .1
+    # Make certain that we have masked over 90% of the bad pixels
+    assert len(b_not_in_a) / len(b) < .1
+
+if __name__ == '__main__':
+    import nose
+    nose.runmodule(argv=['-s', '--with-doctest'], exit=False)
