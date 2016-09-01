d8400d88c874e7604356db7586fa9c9062541598
Thomas A Caswell <tcaswell@bnl.gov>
2016-09-01 16:19:38 -0400

    DOC: remove bootstrap and Napoleon usage

--- doc/conf.py
+++ doc/conf.py
@@ -13,7 +13,6 @@
 
 import sys
 import os
-import sphinx_bootstrap_theme
 import matplotlib as mpl
 mpl.use("Agg")
 
@@ -33,7 +32,6 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.coverage',
               'sphinx.ext.mathjax',
               'sphinx.ext.autosummary',
-              'sphinxcontrib.napoleon',
               'sphinx.ext.extlinks',
               'plot_generator',
               'numpydoc',
@@ -119,20 +117,13 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'bootstrap'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-                      'source_link_position': "footer",
-                      'bootswatch_theme': "spacelab",
-                      'navbar_sidebarrel': False,
-                      'bootstrap_version': "3",
-                      'navbar_links': [("Tutorial", "tutorial")],
-                     }
+html_theme_options = {}
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
