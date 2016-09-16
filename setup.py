269a035222258e93b463851c20f86eb51a451a64
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-09-16 03:00:30 -0400

    setup: Remove unused read() function
    This commit removed code obsoleted by c12b82e (MNT: Updated setup.py)

--- setup.py
+++ setup.py
@@ -4,19 +4,8 @@ import setuptools
 from distutils.core import setup, Extension
 import versioneer
 import numpy as np
-import os
 import sys
 from Cython.Build import cythonize
-
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 
 def c_ext():
     if os.name == 'nt':
