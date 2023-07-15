98cd788f54f0886008ea7f1194365ba94855d046
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2023-07-15 09:52:13 -0400

    DOC: removed old make files for docs

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -2,5 +2,7 @@
 set -e # exit with nonzero exit code if anything fails
 
 make clean
+# The original Makefile was doing nothing if the option 'notebooks' was selected.
+# So this functionality was not supported (or broken)
 # make notebooks
 make html
